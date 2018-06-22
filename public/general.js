fd27839f4989a72d3d87aa1e3a5a93e586a54470
Veelkoov <@veelkoov>
2018-06-22 13:33:08 +0200

    Load data statically, no AJAX; improvements for crawlers

--- public/general.js
+++ public/general.js
@@ -4,36 +4,24 @@ var selectedCountries = [];
 var COUNTRY_COL_IDX = 1;
 
 $(document).ready(function () {
-    initDataTable();
+    $dataTable = $('#artisans').DataTable({"paging": false});
+    initSearchForm();
     initWhatsThis();
 });
-
-function initDataTable() {
-    $.ajax({
-        url: 'data.json',
-        success: function (artisansData) {
-            insertDataTableRows(artisansData);
-            initSearchForm(artisansData);
-
-            $dataTable = $('#artisans').DataTable({"paging": false});
-        }
-    });
-}
 
 function applyFilters() {
     selectedCountries = $countryCheckboxes
         .filter(':checked')
-        .map(function (idx, el) { return getCountryId(el.value); })
+        .map(function (_, node) { return node.value; })
         .get();
 
     $dataTable.draw();
 }
 
-function initSearchForm(artisansData) {
-    var countryCodes = getCountryCodes(artisansData);
+function initSearchForm() {
     var $countriesFilter = $('#countriesFilter')
 
-    $.each(countryCodes, function (idx, countryCode) {
+    $.each(getCountryCodes(), function (idx, countryCode) {
         $countriesFilter.append(
             '<label for="filter-' + countryCode + '">' +
             '   <span class="flag-icon flag-icon-' + countryCode + '"></span>' +
@@ -57,54 +45,20 @@ function initSearchForm(artisansData) {
     });
 }
 
-function getCountryCodes(artisansData) {
-    return $.map(artisansData, function (val, i) { return val.country.toLowerCase(); })
+function getCountryCodes() {
+    return $dataTable
+        .column(COUNTRY_COL_IDX)
+        .data()
         .sort()
-        .filter((v, i, a) => a.indexOf(v) == i && v);
+        .unique()
+        .filter((value, _) => value);
 }
 
 function initWhatsThis() {
     $('.what_link').click(function (ev) {
-        $('#what h1').html(function () {
-            var hour = new Date().getHours();
-            return 'Good ' + (hour >= 4 && hour < 12 ? 'Morning' : hour >= 12 && hour < 20 ? 'Afternoon' : 'Evening') + '!';
-        });
-
         $('#what').toggle(500);
         ev.preventDefault();
     });
-}
-
-function insertDataTableRows(artisansData) {
-    var $artisans = $('#artisans tbody');
-
-    $.each(artisansData, function (idx, artisan) {
-        $artisans.append(getDataTableRow(artisan));
-    });
-}
-
-function getDataTableRow(artisan) {
-    return '<tr>' +
-        '<td class="text-left">' + artisan.name +
-        (artisan.country ? ' <span class="flag-icon flag-icon-' + artisan.country.toLowerCase() + '"></span>' : '') +
-        '</td>' +
-        '<td class="hidden_data">' + getCountryId(artisan.country) + '</td>' +
-        '<td class="text-left">' + artisan.types + '</td>' +
-        '<td>' + getLinkCode(artisan.websiteUrl, '<i class="fas fa-link"></i>') + '</td>' +
-        '<td>' + getLinkCode(artisan.furAffinityUrl, '<img src="FurAffinity.svg"/>') + '</td>' +
-        '<td>' + getLinkCode(artisan.deviantArtUrl, '<i class="fab fa-deviantart"></i>') + '</td>' +
-        '<td>' + getLinkCode(artisan.twitterUrl, '<i class="fab fa-twitter"></i>') + '</td>' +
-        '<td>' + getLinkCode(artisan.facebookUrl, '<i class="fab fa-facebook"></i>') + '</td>' +
-        '<td>' + getLinkCode(artisan.tumblrUrl, '<i class="fab fa-tumblr"></i>') + '</td>' +
-        '</tr>';
-}
-
-function getLinkCode(href, contents) {
-    if (!href) {
-        return '';
-    }
-
-    return '<a href="' + href + '">' + contents + '</a>';
 }
 
 function getCountryId(countryCode) {
