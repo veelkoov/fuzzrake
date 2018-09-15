cc962d590330a30349cb5cdf724f380c4c9bf182
Veelkoov <@veelkoov>
2018-09-15 22:58:13 +0200

    Added total % in stats; deprecation fixes

--- src/Controller/Frontend/DefaultController.php
+++ src/Controller/Frontend/DefaultController.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\Frontend;
 
 use App\Repository\ArtisanRepository;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
