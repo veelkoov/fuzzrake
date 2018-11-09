c7372d9de6029f8a8bfc881551e75915c4a9ef1b
Veelkoov <@veelkoov>
2018-11-09 22:50:31 +0100

    webpack.config.js updates

--- webpack.config.js
+++ webpack.config.js
@@ -19,27 +19,24 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
     .addEntry('main', './assets/js/main.js')
+    //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
+
     .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
 
-    // enables Sass/SCSS support
-    //.enableSassLoader()
-    .enableLessLoader()
-
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
+
+    // uncomment if you use Sass/SCSS files
+    //.enableSassLoader()
+    .enableLessLoader()
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
