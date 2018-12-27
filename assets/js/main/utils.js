cd2ec4c36e86e5340811286d19c73f389771b79a
Veelkoov <@veelkoov>
2018-12-27 21:00:17 +0100

    Data form v4, more data pre-filled

--- assets/js/main/utils.js
+++ assets/js/main/utils.js
@@ -28,14 +28,20 @@ function getGoogleFormPrefilledUrl(artisan) {
     dataItems.push(toDataItem(1452524703, artisan.country));
     dataItems.push(toDataItem(355015034, artisan.state));
     dataItems.push(toDataItem(944749751, artisan.city));
+    dataItems.push(toDataItem(743737005, artisan.paymentPlans));
+    dataItems.push(toDataItem(2034494235, artisan.pricesUrl));
+    dataItems.push(toDataItem(838362497, artisan.productionModel));
     dataItems.push(toDataItem(129031545, artisan.styles));
-    dataItems.push(toDataItem(1324232796, artisan.otherStyles.join('; ')));
+    dataItems.push(toDataItem(1324232796, artisan.otherStyles.join('\n')));
     dataItems.push(toDataItem(1319815626, artisan.types));
-    dataItems.push(toDataItem(67316802, artisan.otherTypes.join('; ')));
+    dataItems.push(toDataItem(67316802, artisan.otherTypes.join('\n')));
     dataItems.push(toDataItem(1197078153, artisan.features));
-    dataItems.push(toDataItem(175794467, artisan.otherFeatures.join('; ')));
+    dataItems.push(toDataItem(175794467, artisan.otherFeatures.join('\n')));
+    dataItems.push(toDataItem(416913125, artisan.speciesDoes));
+    dataItems.push(toDataItem(1335617718, artisan.speciesDoesnt));
     dataItems.push(toDataItem(1291118884, artisan.fursuitReviewUrl));
     dataItems.push(toDataItem(1753739667, artisan.websiteUrl));
+    dataItems.push(toDataItem(110608078, artisan.faqUrl));
     dataItems.push(toDataItem(1781081038, artisan.furAffinityUrl));
     dataItems.push(toDataItem(591054015, artisan.deviantArtUrl));
     dataItems.push(toDataItem(151172280, artisan.twitterUrl));
@@ -44,12 +50,15 @@ function getGoogleFormPrefilledUrl(artisan) {
     dataItems.push(toDataItem(696741203, artisan.instagramUrl));
     dataItems.push(toDataItem(618562986, artisan.youtubeUrl));
     dataItems.push(toDataItem(1355429885, artisan.commisionsQuotesCheckUrl));
+    dataItems.push(toDataItem(1507707399, artisan.otherUrls));
+    dataItems.push(toDataItem(528156817, artisan.languages));
+    dataItems.push(toDataItem(927668258, artisan.makerId));
     dataItems.push(toDataItem(1671817601, artisan.notes));
     dataItems.push(toDataItem(725071599, artisan.intro));
     dataItems.push('entry.1898509469=Yes, I\'m not on the list yet, or I used the update link');
 
     // TODO: get form link form czpcz
-    return 'https://docs.google.com/forms/d/e/1FAIpQLScjKIP1B2zrLi1TkA0QLDGuPaPM5GkYFbd6jw8etd4h0zDHPg/viewform?usp=pp_url&' + dataItems.join('&');
+    return 'https://docs.google.com/forms/d/e/1FAIpQLSd4N7m7Sga67O7jzUGuvTg6ZpFcMxQ0HtsZSkCOTSgiLBRwfQ/viewform?usp=pp_url&' + dataItems.join('&');
 }
 
 export function updateUpdateRequestData(divId, artisan) {
