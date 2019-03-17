3aa3990f0ed9ce1ed7b9baff0871e73e76dfc18f
Veelkoov <@veelkoov>
2019-03-17 14:27:13 +0100

    Ansible improvements: DB download option; extracted SG update; refactor

--- bin/dump-db.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pushd "$(dirname "$0")/../ansible"
-
-ansible-playbook dump-db.yaml "${@:1}"
--- bin/update-db.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pushd "$(dirname "$0")/../ansible"
-
-ansible-playbook update-db.yaml "${@:1}"
