6a5a1db73b96d7d21c8d5e8801e84f4139cb2f7f
Veelkoov <@veelkoov>
2019-04-06 12:51:49 +0200

    Removed link to FA on the last image; changed margins in full info modal view

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,12 +1,3 @@
 'use strict';
 
 require('../css/general.less');
-
-import $ from 'jquery';
-
-$(function () {
-    $('a.art-link').click(function (evt) {
-        window.open(this.href);
-        evt.preventDefault();
-    });
-});
