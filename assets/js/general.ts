c850c7fa12c1f5dfe4ea63eb631e65c34bd7db9a
Veelkoov <@veelkoov>
2019-07-18 00:30:32 +0200

    Improved event details presentation

---
+++ assets/js/general.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+require('../css/general.less');
+
+function scrollPastMenuBarOnHash(): void {
+    if (window.location.hash) {
+        scrollBy(0, -70);
+    }
+}
+
+$(window).on('hashchange', scrollPastMenuBarOnHash);
+$(scrollPastMenuBarOnHash);
