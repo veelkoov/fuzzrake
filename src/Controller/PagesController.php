a965d935eef67d0e51c4f25511ba209142e845f1
Veelkoov <@veelkoov>
2019-09-15 15:26:38 +0200

    Unused repository

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
