3820d29de9cad6f6e6e2541ad8d78e57aa605f6f
Veelkoov <@veelkoov>
2019-10-28 20:31:40 +0100

    Improved Parse

--- src/Utils/Parse.php
+++ src/Utils/Parse.php
@@ -10,13 +10,15 @@ class Parse
     {
     }
 
-    public static function tInt(string $input): int
+    public static function tInt(?string $input): int
     {
-        return self::int(trim($input));
+        return self::int(trim($input ?? ''));
     }
 
-    public static function int(string $input): int
+    public static function int(?string $input): int
     {
+        $input = $input ?? '';
+
         $result = (int) $input;
 
         if ((string) $result !== $input) {
@@ -26,13 +28,15 @@ class Parse
         return $result;
     }
 
-    public static function tPercentAsInt(string $input): int
+    public static function tPercentAsInt(?string $input): int
     {
-        return self::percentAsInt(trim($input));
+        return self::percentAsInt(trim($input ?? ''));
     }
 
-    public static function percentAsInt(string $input): int
+    public static function percentAsInt(?string $input): int
     {
+        $input = $input ?? '';
+
         $result = (int) substr($input, 0, -1);
 
         if ((string) $result.'%' !== $input) {
@@ -42,20 +46,23 @@ class Parse
         return $result;
     }
 
-    public static function tFloat(string $input): float
+    public static function tFloat(?string $input): float
     {
-        return self::float(trim($input));
+        return self::float(trim($input ?? ''));
     }
 
-    public static function float(string $input): float
+    public static function float(?string $input): float
     {
+        $input = $input ?? '';
+
         $result = (float) $input;
 
         if ('-' === substr($input, 0, 1)) {
             $input = substr($input, 1);
         }
 
-        if (false !== strpos($input, '.') && (strlen($input) < 3 || '.' !== trim($input, '1234567890'))) {
+        if (trim($input, '.') !== $input || 0 === strlen($input)
+            || '' !== trim($input, '1234567890.') || substr_count($input, '.') > 1) {
             throw new ParseException("'$input' is not a valid float");
         }
 
--- tests/Utils/ParseTest.php
+++ tests/Utils/ParseTest.php
@@ -20,13 +20,13 @@ class ParseTest extends TestCase
     public function testIntAndTInt($input, $expectedInt, $expectedTInt): void
     {
         try {
-            self::assertEquals($expectedInt, Parse::int($input));
+            self::assertSame($expectedInt, Parse::int($input));
         } catch (ParseException $e) {
             self::assertFalse($expectedInt);
         }
 
         try {
-            self::assertEquals($expectedTInt, Parse::tInt($input));
+            self::assertSame($expectedTInt, Parse::tInt($input));
         } catch (ParseException $e) {
             self::assertFalse($expectedTInt);
         }
@@ -42,13 +42,13 @@ class ParseTest extends TestCase
     public function testFloatAndTFloat($input, $expectedFloat, $expectedTFloat): void
     {
         try {
-            self::assertEquals($expectedFloat, Parse::float($input));
+            self::assertSame($expectedFloat, Parse::float($input));
         } catch (ParseException $e) {
             self::assertFalse($expectedFloat);
         }
 
         try {
-            self::assertEquals($expectedTFloat, Parse::tFloat($input));
+            self::assertSame($expectedTFloat, Parse::tFloat($input));
         } catch (ParseException $e) {
             self::assertFalse($expectedTFloat);
         }
@@ -64,13 +64,13 @@ class ParseTest extends TestCase
     public function testPercentAsIntAndTPercentAsInt($input, $expectedPercentAsInt, $expectedTPercentAsInt): void
     {
         try {
-            self::assertEquals($expectedPercentAsInt, Parse::percentAsInt($input));
+            self::assertSame($expectedPercentAsInt, Parse::percentAsInt($input));
         } catch (ParseException $e) {
             self::assertFalse($expectedPercentAsInt);
         }
 
         try {
-            self::assertEquals($expectedTPercentAsInt, Parse::tPercentAsInt($input));
+            self::assertSame($expectedTPercentAsInt, Parse::tPercentAsInt($input));
         } catch (ParseException $e) {
             self::assertFalse($expectedTPercentAsInt);
         }
@@ -79,6 +79,9 @@ class ParseTest extends TestCase
     public function intDataProvider(): array
     {
         return [
+            [null, false, false],
+            ['', false, false],
+            [' ', false, false],
             ['0', 0, 0],
             ['0 ', false, 0],
             ['1', 1, 1],
@@ -95,6 +98,9 @@ class ParseTest extends TestCase
     public function percentAsIntDataProvider(): array
     {
         return [
+            [null, false, false],
+            ['', false, false],
+            [' ', false, false],
             ['0%', 0, 0],
             ['0% ', false, 0],
             ['1%', 1, 1],
@@ -111,6 +117,9 @@ class ParseTest extends TestCase
     public function floatDataProvider(): array
     {
         return [
+            [null, false, false],
+            ['', false, false],
+            [' ', false, false],
             ['0', 0.0, 0.0],
             ['0 ', false, 0.0],
             ['1.0', 1.0, 1.0],
@@ -120,6 +129,17 @@ class ParseTest extends TestCase
             ['-1.0 ', false, -1.0],
             ['1.', false, false],
             ['.1', false, false],
+            ['e', false, false],
+            ['-', false, false],
+            ['-e', false, false],
+            ['-.', false, false],
+            ['.-', false, false],
+            ['0.-', false, false],
+            ['.0-', false, false],
+            ['.-0', false, false],
+            ['.0-0', false, false],
+            ['0.0-', false, false],
+            ['0.0-0', false, false],
         ];
     }
 }
