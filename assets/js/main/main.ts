01b35588aae4080b3c649e6b804a65dee95c588c
Veelkoov <@veelkoov>
2019-11-10 23:01:05 +0100

    Fetch data through API; fetch maker IDs map as well

--- assets/js/main/main.ts
+++ assets/js/main/main.ts
@@ -9,13 +9,16 @@ import {makerIdHashRegexp} from "../consts";
 
 require('../../3rd-party/flag-icon-css/css/flag-icon.css');
 
-function initRequestUpdateModal() {
+declare var ARTISANS: Artisan[];
+declare var MAKER_IDS_MAP: {string: string};
+
+function initRequestUpdateModal(): void {
     $('#updateRequestModal').on('show.bs.modal', function (event) {
         updateRequestUpdateModalWithRowData($(event.relatedTarget).closest('tr').data('artisan'));
     });
 }
 
-function addReferrerRequestTooltip() {
+function addReferrerRequestTooltip(): void {
     $('div.artisan-links')
         .attr('title', 'If you\'re going to contact the studio/maker, <u>please let them' +
             ' know you found them here</u>! This will help us all a lot. Thank you!')
@@ -26,29 +29,27 @@ function addReferrerRequestTooltip() {
         .tooltip();
 }
 
-function updateRequestUpdateModalWithRowData(artisan) {
+function updateRequestUpdateModalWithRowData(artisan): void {
     $('#artisanNameUR').html(artisan.name);
 
     Utils.updateUpdateRequestData('updateRequestSingle', artisan);
 }
 
-function openArtisanByFragment(hash: string) {
+function openArtisanByFragment(hash: string): void {
     if (hash.match(makerIdHashRegexp)) {
         $(hash).children().eq(0).trigger('click');
     }
 }
 
-function scrollToTopOfTheDataTable() {
+function scrollToTopOfTheDataTable(): void {
     // Anti-scam warning causes the user to be at the bottom of the table
     window.scrollTo(0, $('#data-table-container').offset().top - 70); // FIXME: 70!!!
 }
 
-export function init() {
-    $('#scam-risk-acknowledgement').on('click', (event) => {
-        $('#scam-risk-warning, #scam-risk-acknowledged').toggle();
-        scrollToTopOfTheDataTable();
-        event.preventDefault();
-    });
+function initAfterDataLoaded(): void {
+    if (null === MAKER_IDS_MAP || null === ARTISANS) {
+        return;
+    }
 
     DataTable.init();
     DetailsModal.init();
@@ -61,4 +62,41 @@ export function init() {
     $('#data-loading-message, #data-table-container').toggle();
 }
 
-export {Artisan};
+function loadArtisanData(artisansApiUrl: string): void {
+    $.ajax(artisansApiUrl, {
+        dataType: 'json',
+        error: jqXHR => {
+            alert('Failed to load data');
+        },
+        success: data => {
+            ARTISANS = data;
+            initAfterDataLoaded();
+        }
+    });
+}
+
+function loadMakerIdMapData(makerIdsApiUrl: string): void {
+    $.ajax(makerIdsApiUrl, {
+        dataType: 'json',
+        error: jqXHR => {
+            alert('Failed to load data');
+        },
+        success: data => {
+            MAKER_IDS_MAP = data;
+            initAfterDataLoaded();
+        }
+    });
+}
+
+function init(artisansApiUrl: string, makerIdsApiUrl: string): void {
+    $('#scam-risk-acknowledgement').on('click', (event) => {
+        $('#scam-risk-warning, #scam-risk-acknowledged').toggle();
+        scrollToTopOfTheDataTable();
+        event.preventDefault();
+    });
+
+    loadArtisanData(artisansApiUrl);
+    loadMakerIdMapData(makerIdsApiUrl);
+}
+
+export {Artisan, init};
