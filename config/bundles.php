dfdb855441497d4d4091ab851fd9a327058f40e2
Veelkoov <@veelkoov>
2020-01-02 20:20:30 +0100

    Further requires

--- config/bundles.php
+++ config/bundles.php
@@ -2,13 +2,12 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class                => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class                 => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class                          => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class               => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class     => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class                    => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class                        => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class            => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class                    => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class                 => ['all' => true],
 ];
