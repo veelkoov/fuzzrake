b639ded5a3c70d71dbf33179caf13f88e2b20493
Veelkoov <@veelkoov>
2020-01-16 21:47:45 +0100

    Removed test for removed endpoint

--- tests/Controller/CrawlersControllerTest.php
+++ tests/Controller/CrawlersControllerTest.php
@@ -8,15 +8,6 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CrawlersControllerTest extends WebTestCase
 {
-    public function testRobots()
-    {
-        $client = static::createClient();
-
-        $client->request('GET', '/robots.txt');
-
-        static::assertEquals(200, $client->getResponse()->getStatusCode());
-    }
-
     public function testSitemap()
     {
         $client = static::createClient();
