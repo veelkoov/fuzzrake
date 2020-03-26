64fb88ccc59cf285637a69aa1b0ea84f0d2318b4
Veelkoov <@veelkoov>
2020-03-26 20:38:51 +0100

    State column + state filter + improvements in Fixers + added json ext + removed obsolete function

--- src/Utils/Data/Fixer/CountryFixer.php
+++ src/Utils/Data/Fixer/CountryFixer.php
@@ -4,25 +4,10 @@ declare(strict_types=1);
 
 namespace App\Utils\Data\Fixer;
 
-use App\Utils\Regexp\Regexp;
-
-class CountryFixer implements FixerInterface
+class CountryFixer extends AbstractStringFixer
 {
-    /**
-     * @var string[]
-     */
-    private array $replacements;
-
     public function __construct(array $countries)
     {
-        $this->replacements = $countries['replacements'];
-    }
-
-    public function fix(string $fieldName, string $subject): string
-    {
-        $subject = trim($subject);
-        $subject = Regexp::replaceAll($this->replacements, $subject, '#^', '$#i');
-
-        return $subject;
+        parent::__construct($countries);
     }
 }
