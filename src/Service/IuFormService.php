813b13b911720ac4b0c71201627ecbe60b374bf3
Veelkoov <@veelkoov>
2020-07-30 22:03:51 +0200

    Send notifications about IU form submissions

--- src/Service/IuFormService.php
+++ src/Service/IuFormService.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Entity\Artisan;
+use App\Utils\Artisan\Fields;
 use App\Utils\DateTime\DateTimeUtils;
 use App\Utils\Json;
+use App\Utils\StrUtils;
 use Exception;
 use InvalidArgumentException;
 use Psr\Log\LoggerInterface;
@@ -19,17 +21,24 @@ class IuFormService
     private string $dataDirPath;
     private Filesystem $filesystem;
     private string $s3CopiesBucketUrl;
+    private string $notificationsSnsTopicArn;
 
-    public function __construct(LoggerInterface $logger, string $iuFormDataDirPath, string $iuFormDataS3CopiesBucketUrl)
-    {
+    public function __construct(LoggerInterface $logger, string $iuFormDataDirPath, string $iuFormDataS3CopiesBucketUrl,
+        string $notificationSnsTopicArn
+    ) {
         $this->filesystem = new Filesystem();
 
         $this->logger = $logger;
         $this->dataDirPath = $iuFormDataDirPath;
         $this->s3CopiesBucketUrl = $iuFormDataS3CopiesBucketUrl;
+        $this->notificationsSnsTopicArn = $notificationSnsTopicArn;
 
-        if (pattern('^s3://[-.a-z0-9]+$')->fails($iuFormDataS3CopiesBucketUrl)) {
+        if (pattern('^s3://[-.a-z0-9]+$', 'i')->fails($iuFormDataS3CopiesBucketUrl)) {
             throw new InvalidArgumentException("$iuFormDataS3CopiesBucketUrl is not a valid S3 bucket URL");
+        }
+
+        if (pattern('^arn:aws:sns:[-a-z0-9]+:\d+:[-_a-z0-9]+$', 'i')->fails($notificationSnsTopicArn)) {
+            throw new InvalidArgumentException("$notificationSnsTopicArn is not a valid SNS topic ARN");
         }
     }
 
@@ -40,7 +49,7 @@ class IuFormService
 
             $filePath = $this->saveOnDisk($jsonData);
             $this->sendCopyToS3($filePath);
-            $this->notifyAboutSubmission($filePath, $data);
+            $this->notifyAboutSubmission($data);
 
             return true;
         } catch (Exception $exception) {
@@ -68,9 +77,34 @@ class IuFormService
         return $filePath;
     }
 
+    private function notifyAboutSubmission(Artisan $data): void
+    {
+        $names = StrUtils::artisanNamesSafeForCli($data);
+        $message = <<<MESSAGE
+            {$names}
+            From: {$data->getCountry()}
+            
+            MESSAGE;
+
+        foreach (Fields::urls() as $url) {
+            if (($val = $data->get($url))) {
+                $message .= $url->name().': '.$val."\n";
+            }
+        }
+
+        $this->runAwsCliCmd(['aws', 'sns', 'publish', '--topic-arn', $this->notificationsSnsTopicArn,
+            '--subject', "IU submission: {$data->getName()}", '--message', $message, ], 'Sending copy to S3');
+    }
+
     private function sendCopyToS3(string $filePath): void
     {
-        $process = new Process(['aws', 's3', 'cp', $filePath, $this->s3CopiesBucketUrl]);
+        $this->runAwsCliCmd(['aws', 's3', 'cp', $filePath, $this->s3CopiesBucketUrl],
+            'Sending copy to S3');
+    }
+
+    private function runAwsCliCmd(array $command, string $description): void
+    {
+        $process = new Process($command);
         $process->mustRun();
 
         $context = [
@@ -79,15 +113,10 @@ class IuFormService
         ];
 
         if ($process->isSuccessful()) {
-            $this->logger->info('Sent copy to S3', $context);
+            $this->logger->info("$description successful", $context);
         } else {
             // TODO: Consider throwing up
-            $this->logger->error('Failed sending copy to S3', $context);
+            $this->logger->error("$description failed", $context);
         }
-    }
-
-    private function notifyAboutSubmission(string $filePath, Artisan $data): void
-    {
-        // TODO
     }
 }
