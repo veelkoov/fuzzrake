caf0c09a3905ac3c071a65102abf2fc86b24db7e
Veelkoov <@veelkoov>
2020-08-12 22:31:25 +0200

    Step on Add/update already asks them to check

--- src/Controller/IuFormController.php
+++ src/Controller/IuFormController.php
@@ -32,8 +32,6 @@ class IuFormController extends AbstractRecaptchaBackedController
             throw $this->createNotFoundException('Failed to find a maker with given ID');
         }
 
-        // TODO: New maker - require them to check the list
-
         $form = $this->createForm(IuForm::class, $artisan);
         $form->handleRequest($request);
 
