4f8a647bc388f008f2c0ed4485a08a09d9cd320e
Veelkoov <@veelkoov>
2020-08-12 23:16:39 +0200

    Improved function name

--- assets/scripts/main/utils.ts
+++ assets/scripts/main/utils.ts
@@ -3,7 +3,7 @@ import DataBridge from "../class/DataBridge";
 
 export function updateUpdateRequestData(divId: string, artisan: Artisan): void {
     jQuery(`#${divId} .twitterUrl`).attr('href', getTwitterGuestRequestUrl(artisan));
-    jQuery(`#${divId} .artisanGoogleFormUrl`).attr('href', getMakerUpdatePrefilledUrl(artisan));
+    jQuery(`#${divId} .artisanGoogleFormUrl`).attr('href', getIuFormLinkForArtisan(artisan));
     jQuery(`#${divId} .guestGoogleFormUrl`).attr('href', getGuestRequestPrefilledUrl(artisan));
 }
 
@@ -11,7 +11,7 @@ function getTwitterGuestRequestUrl(artisan: Artisan) {
     return 'https://twitter.com/intent/tweet?original_referer=http%3A%2F%2Fgetfursu.it%2F&ref_src=twsrc%5Etfw&screen_name=Veelkoov&text=Fursuit%20maker%20update%20request%3A%20' + encodeURIComponent(artisan.name) + '%20(please%20describe%20details)&tw_p=tweetbutton';
 }
 
-function getMakerUpdatePrefilledUrl(artisan: Artisan): string { // TODO: rename
+function getIuFormLinkForArtisan(artisan: Artisan): string {
     return DataBridge.getIuFormRedirectUrl().replace('MAKER_ID', artisan.getLastMakerId())
 }
 
