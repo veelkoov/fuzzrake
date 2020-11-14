9de138c5e86d2f8f31444821896429c5a1867c02
Veelkoov <@veelkoov>
2020-11-14 20:04:04 +0100

    UI tests work in Docker; 1st working smoke test

--- tests/Frontend/SmokeUiTest.php
+++ tests/Frontend/SmokeUiTest.php
@@ -8,28 +8,30 @@ use App\Tests\TestUtils\DbEnabledPantherTestCase;
 use Facebook\WebDriver\Exception\WebDriverException;
 use Facebook\WebDriver\WebDriverBy;
 
-class BasicUiTest extends DbEnabledPantherTestCase
+class SmokeUiTest extends DbEnabledPantherTestCase
 {
     /**
      * @throws WebDriverException
      */
-    public function testEverythingLoadsCorrectly(): void
+    public function testMainPageLoadsCorrectly(): void
     {
         $client = static::createPantherClient();
-        $client->request('GET', 'http://localhost:8080/');
-        $client->waitForVisibility('#artisans', 5);
+        $client->request('GET', '/');
+        $client->waitForVisibility('#scam-risk-warning', 5);
+        $client->findElement(WebDriverBy::id('scam-risk-acknowledgement'))->click();
+        $client->waitForVisibility('#artisans', 2);
         $client->findElement(WebDriverBy::id('filtersButton'))->click();
         $client->waitForVisibility('#filtersTitle', 2);
 
         self::assertNotEmpty('We are here, so whole scenario succeeded');
     }
 
-    public function testFilters(): void
+    public function testFiltersWorkInGeneral(): void
     {
         self::assertTrue(true); // TODO
     }
 
-    public function testDetailsModal(): void
+    public function testDetailsModalWorksInGeneral(): void
     {
         self::assertTrue(true); // TODO
     }
