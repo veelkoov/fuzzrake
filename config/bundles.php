52d47dbedd60dc9420da84560266dbb149000c0e
Veelkoov <veelkoov@protonmail.com>
2021-02-21 11:34:33 +0100

    Upgraded to Symfony 5.2

--- config/bundles.php
+++ config/bundles.php
@@ -1,15 +1,13 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class                 => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class                           => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class                => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class                     => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class  => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class                         => ['dev' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class             => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class                     => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class                  => ['all' => true],
-    Symplify\ConsoleColorDiff\ConsoleColorDiffBundle::class               => ['dev' => true, 'test' => true],
-    Symplify\ComposerJsonManipulator\ComposerJsonManipulatorBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class                => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class                          => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class               => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class                        => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class            => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class                    => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class                 => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class                        => ['dev' => true, 'test' => true],
 ];
