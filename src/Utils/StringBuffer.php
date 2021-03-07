10b874554925e322eeedaedf4bdccb5dec2207f2
Veelkoov <veelkoov@protonmail.com>
2021-03-07 09:21:44 +0100

    Allow different token delimiters; added basic tests for StringBuffer

--- src/Utils/StringBuffer.php
+++ src/Utils/StringBuffer.php
@@ -5,15 +5,19 @@ declare(strict_types=1);
 namespace App\Utils;
 
 use RuntimeException;
+use Symfony\Component\String\AbstractString;
+use Symfony\Component\String\UnicodeString;
 use TRegx\SafeRegex\Exception\PregException;
 use TRegx\SafeRegex\preg;
 use UnexpectedValueException;
 
 class StringBuffer
 {
-    public function __construct(
-        private string $buffer,
-    ) {
+    private AbstractString $buffer;
+
+    public function __construct(string $buffer)
+    {
+        $this->buffer = new UnicodeString($buffer);
     }
 
     public function readUntil(string $terminator, bool $trimWhitespaceAfterwards = true): string
@@ -33,29 +37,25 @@ class StringBuffer
 
     public function readToken(): string
     {
-        $expectedEmpty = $this->readUntil('|', false);
+        $terminator = $this->readCharacter();
 
-        if ('' !== $expectedEmpty) {
-            throw new UnexpectedValueException("Unexpected content before | delimiter: '$expectedEmpty'");
-        }
-
-        return $this->readUntil('|');
+        return $this->readUntil($terminator);
     }
 
     public function skipWhitespace(): void
     {
-        $this->buffer = ltrim($this->buffer);
+        $this->buffer = $this->buffer->trimStart();
     }
 
     public function isEmpty(): bool
     {
-        return 0 === strlen($this->buffer);
+        return 0 === $this->buffer->length();
     }
 
     private function readUntilRegexp(string $terminator, bool $trimWhitespaceAfterwards = true): string
     {
         try {
-            $parts = preg::split(pattern($terminator)->delimited(), $this->buffer, 2);
+            $parts = preg::split(pattern($terminator)->delimited(), $this->buffer->toString(), 2);
         } catch (PregException $e) {
             throw new RuntimeException("Terminator '$terminator' is not a valid regexp: {$e->getMessage()}");
         }
@@ -64,12 +64,20 @@ class StringBuffer
             throw new UnexpectedValueException("Unable to find '{$terminator}' in the remaining buffer '{$this->buffer}'");
         }
 
-        $this->buffer = $parts[1];
+        $this->buffer = new UnicodeString($parts[1]);
 
         if ($trimWhitespaceAfterwards) {
             $this->skipWhitespace();
         }
 
         return $parts[0];
+    }
+
+    public function readCharacter(): string
+    {
+        $result = $this->buffer->slice(0, 1)->toString();
+        $this->buffer = $this->buffer->slice(1);
+
+        return $result;
     }
 }
