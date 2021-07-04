523ca5f626909754347e460b7190e217e6158ae8
Veelkoov <veelkoov@protonmail.com>
2021-07-04 20:25:26 +0200

    Reorganized FieldsDefinitions

--- src/Utils/Artisan/FieldsDefinitions.php
+++ src/Utils/Artisan/FieldsDefinitions.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace App\Utils\Artisan;
 
-use App\Utils\Artisan\ValidationRegexps as VR;
+use App\Utils\Artisan\ValidationRegexps as V;
+use App\Utils\Artisan\Fields as F;
 use App\Utils\Traits\UtilityClass;
 
 final class FieldsDefinitions
@@ -12,121 +13,109 @@ final class FieldsDefinitions
     use UtilityClass;
 
     public const FIELDS_ARRAY_DATA = [
-        /*                                                                                    IS PUBLIC? ----------.
-         *                                                                                SHOW IN STATS? -------.  |
-         *                                                                                 IS PERSISTED? ----.  |  |
-         *                                                                                      IS LIST? -.  |  |  |
-         * PRETTY_NAME                     => ['model name (artisan)',     'validation regexp',           V  V  V  V
-         */
-        Fields::MAKER_ID                   => ['makerId',                  VR::MAKER_ID,                  0, 1, 1, 1],
-        Fields::FORMER_MAKER_IDS           => ['formerMakerIds',           VR::FORMER_MAKER_IDS,          1, 1, 1, 1],
-
-        Fields::NAME                       => ['name',                     VR::NON_EMPTY,                 0, 1, 1, 1],
-        Fields::FORMERLY                   => ['formerly',                 VR::ANYTHING,                  1, 1, 1, 1],
-
-        Fields::INTRO                      => ['intro',                    VR::ANYTHING,                  0, 1, 1, 1],
-        Fields::SINCE                      => ['since',                    VR::SINCE,                     0, 1, 1, 1],
-
-        Fields::LANGUAGES                  => ['languages',                VR::ANYTHING,                  1, 1, 1, 1],
-        Fields::COUNTRY                    => ['country',                  VR::COUNTRY,                   0, 1, 1, 1],
-        Fields::STATE                      => ['state',                    VR::ANYTHING,                  0, 1, 1, 1],
-        Fields::CITY                       => ['city',                     VR::ANYTHING,                  0, 1, 1, 1],
-
-        Fields::PRODUCTION_MODELS_COMMENT  => ['productionModelsComment',  VR::ANYTHING,                  0, 1, 0, 1],
-        Fields::PRODUCTION_MODELS          => ['productionModels',         VR::LIST_VALIDATION,           1, 1, 1, 1],
-
-        Fields::STYLES_COMMENT             => ['stylesComment',            VR::ANYTHING,                  0, 1, 0, 1],
-        Fields::STYLES                     => ['styles',                   VR::LIST_VALIDATION,           1, 1, 1, 1],
-        Fields::OTHER_STYLES               => ['otherStyles',              VR::LIST_VALIDATION,           1, 1, 1, 1],
-
-        Fields::ORDER_TYPES_COMMENT        => ['orderTypesComment',        VR::ANYTHING,                  0, 1, 0, 1],
-        Fields::ORDER_TYPES                => ['orderTypes',               VR::LIST_VALIDATION,           1, 1, 1, 1],
-        Fields::OTHER_ORDER_TYPES          => ['otherOrderTypes',          VR::LIST_VALIDATION,           1, 1, 1, 1],
-
-        Fields::FEATURES_COMMENT           => ['featuresComment',          VR::ANYTHING,                  0, 1, 0, 1],
-        Fields::FEATURES                   => ['features',                 VR::LIST_VALIDATION,           1, 1, 1, 1],
-        Fields::OTHER_FEATURES             => ['otherFeatures',            VR::LIST_VALIDATION,           1, 1, 1, 1],
-
-        Fields::PAYMENT_PLANS              => ['paymentPlans',             VR::ANYTHING,                  0, 1, 1, 1],
-        Fields::PAYMENT_METHODS            => ['paymentMethods',           VR::LIST_VALIDATION,           1, 1, 1, 1],
-        Fields::CURRENCIES_ACCEPTED        => ['currenciesAccepted',       VR::LIST_VALIDATION,           1, 1, 1, 1],
-
-        Fields::SPECIES_COMMENT            => ['speciesComment',           VR::ANYTHING,                  0, 1, 1, 1],
-        Fields::SPECIES_DOES               => ['speciesDoes',              VR::ANYTHING,                  1, 1, 1, 1],
-        Fields::SPECIES_DOESNT             => ['speciesDoesnt',            VR::ANYTHING,                  1, 1, 1, 1],
-
-        Fields::URL_FURSUITREVIEW          => ['fursuitReviewUrl',         VR::FSR_URL,                   0, 1, 1, 1],
-        Fields::URL_WEBSITE                => ['websiteUrl',               VR::GENERIC_URL,               0, 1, 1, 1],
-        Fields::URL_PRICES                 => ['pricesUrl',                VR::GENERIC_URL_LIST,          1, 1, 1, 1],
-        Fields::URL_COMMISSIONS            => ['commissionsUrl',           VR::GENERIC_URL_LIST,          1, 1, 1, 1],
-        Fields::URL_FAQ                    => ['faqUrl',                   VR::GENERIC_URL,               0, 1, 1, 1],
-        Fields::URL_FUR_AFFINITY           => ['furAffinityUrl',           VR::FA_URL,                    0, 1, 1, 1],
-        Fields::URL_DEVIANTART             => ['deviantArtUrl',            VR::DA_URL,                    0, 1, 1, 1],
-        Fields::URL_TWITTER                => ['twitterUrl',               VR::TWITTER_URL,               0, 1, 1, 1],
-        Fields::URL_FACEBOOK               => ['facebookUrl',              VR::FACEBOOK_URL,              0, 1, 1, 1],
-        Fields::URL_TUMBLR                 => ['tumblrUrl',                VR::TUMBLR_URL,                0, 1, 1, 1],
-        Fields::URL_INSTAGRAM              => ['instagramUrl',             VR::INSTAGRAM_URL,             0, 1, 1, 1],
-        Fields::URL_YOUTUBE                => ['youtubeUrl',               VR::YOUTUBE_URL,               0, 1, 1, 1],
-        Fields::URL_LINKLIST               => ['linklistUrl',              VR::GENERIC_URL,               0, 1, 1, 1],
-        Fields::URL_FURRY_AMINO            => ['furryAminoUrl',            VR::GENERIC_URL,               0, 1, 1, 1],
-        Fields::URL_ETSY                   => ['etsyUrl',                  VR::GENERIC_URL,               0, 1, 1, 1],
-        Fields::URL_THE_DEALERS_DEN        => ['theDealersDenUrl',         VR::GENERIC_URL,               0, 1, 1, 1],
-        Fields::URL_OTHER_SHOP             => ['otherShopUrl',             VR::GENERIC_URL,               0, 1, 1, 1],
-        Fields::URL_QUEUE                  => ['queueUrl',                 VR::GENERIC_URL,               0, 1, 1, 1],
-        Fields::URL_SCRITCH                => ['scritchUrl',               VR::SCRITCH_URL,               0, 1, 1, 1],
-        Fields::URL_FURTRACK               => ['furtrackUrl',              VR::FURTRACK_URL,              0, 1, 1, 1],
-        Fields::URL_PHOTOS                 => ['photoUrls',                VR::PHOTO_URL_LIST,            1, 1, 1, 1],
-        Fields::URL_MINIATURES             => ['miniatureUrls',            VR::MINIATURE_URL_LIST,        1, 1, 1, 1],
-        Fields::URL_OTHER                  => ['otherUrls',                VR::ANYTHING,                  0, 1, 1, 1],
-
-        Fields::NOTES                      => ['notes',                    VR::ANYTHING,                  0, 1, 0, 1],
-        Fields::INACTIVE_REASON            => ['inactiveReason',           VR::ANYTHING,                  0, 1, 1, 1],
-        Fields::CS_LAST_CHECK              => ['csLastCheck',              VR::ANYTHING,                  0, 0, 0, 1],
-        Fields::CS_TRACKER_ISSUE           => ['csTrackerIssue',           VR::ANYTHING,                  0, 1, 0, 1],
-        Fields::BP_LAST_CHECK              => ['bpLastCheck',              VR::ANYTHING,                  0, 0, 0, 1],
-        Fields::BP_TRACKER_ISSUE           => ['bpTrackerIssue',           VR::ANYTHING,                  0, 1, 0, 1],
-        Fields::OPEN_FOR                   => ['openFor',                  VR::ANYTHING,                  1, 0, 0, 1],
-        Fields::CLOSED_FOR                 => ['closedFor',                VR::ANYTHING,                  1, 0, 0, 1],
-        Fields::COMPLETENESS               => ['completeness',             VR::ANYTHING,                  0, 0, 0, 1],
-
-        Fields::CONTACT_ALLOWED            => ['contactAllowed',           VR::ANYTHING,                  0, 1, 0, 1],
-        Fields::CONTACT_METHOD             => ['contactMethod',            VR::ANYTHING,                  0, 1, 0, 0],
-        Fields::CONTACT_ADDRESS_PLAIN      => ['contactAddressPlain',      VR::ANYTHING,                  0, 1, 0, 0],
-        Fields::CONTACT_INFO_ORIGINAL      => ['contactInfoOriginal',      VR::ANYTHING,                  0, 1, 0, 0],
-        Fields::CONTACT_INFO_OBFUSCATED    => ['contactInfoObfuscated',    VR::ANYTHING,                  0, 1, 0, 1],
-
-        Fields::PASSWORD                   => ['password',                 VR::ANYTHING,                  0, 1, 0, 0],
+    //
+    //                         IS PUBLIC? ----------.
+    //                     SHOW IN STATS? -------.  |
+    //                      IS PERSISTED? ----.  |  |
+    //                           IS LIST? -.  |  |  |
+    //  PRETTY_NAME                        V  V  V  V  model name (artisan)        validation regexp
+    //
+        F::MAKER_ID                   => [ 0, 1, 1, 1, 'makerId',                  V::MAKER_ID ],
+        F::FORMER_MAKER_IDS           => [ 1, 1, 1, 1, 'formerMakerIds',           V::FORMER_MAKER_IDS ],
+        F::NAME                       => [ 0, 1, 1, 1, 'name',                     V::NON_EMPTY ],
+        F::FORMERLY                   => [ 1, 1, 1, 1, 'formerly',                 V::ANYTHING ],
+        F::INTRO                      => [ 0, 1, 1, 1, 'intro',                    V::ANYTHING ],
+        F::SINCE                      => [ 0, 1, 1, 1, 'since',                    V::SINCE ],
+        F::LANGUAGES                  => [ 1, 1, 1, 1, 'languages',                V::ANYTHING ],
+        F::COUNTRY                    => [ 0, 1, 1, 1, 'country',                  V::COUNTRY ],
+        F::STATE                      => [ 0, 1, 1, 1, 'state',                    V::ANYTHING ],
+        F::CITY                       => [ 0, 1, 1, 1, 'city',                     V::ANYTHING ],
+        F::PRODUCTION_MODELS_COMMENT  => [ 0, 1, 0, 1, 'productionModelsComment',  V::ANYTHING ],
+        F::PRODUCTION_MODELS          => [ 1, 1, 1, 1, 'productionModels',         V::LIST_VALIDATION ],
+        F::STYLES_COMMENT             => [ 0, 1, 0, 1, 'stylesComment',            V::ANYTHING ],
+        F::STYLES                     => [ 1, 1, 1, 1, 'styles',                   V::LIST_VALIDATION ],
+        F::OTHER_STYLES               => [ 1, 1, 1, 1, 'otherStyles',              V::LIST_VALIDATION ],
+        F::ORDER_TYPES_COMMENT        => [ 0, 1, 0, 1, 'orderTypesComment',        V::ANYTHING ],
+        F::ORDER_TYPES                => [ 1, 1, 1, 1, 'orderTypes',               V::LIST_VALIDATION ],
+        F::OTHER_ORDER_TYPES          => [ 1, 1, 1, 1, 'otherOrderTypes',          V::LIST_VALIDATION ],
+        F::FEATURES_COMMENT           => [ 0, 1, 0, 1, 'featuresComment',          V::ANYTHING ],
+        F::FEATURES                   => [ 1, 1, 1, 1, 'features',                 V::LIST_VALIDATION ],
+        F::OTHER_FEATURES             => [ 1, 1, 1, 1, 'otherFeatures',            V::LIST_VALIDATION ],
+        F::PAYMENT_PLANS              => [ 0, 1, 1, 1, 'paymentPlans',             V::ANYTHING ],
+        F::PAYMENT_METHODS            => [ 1, 1, 1, 1, 'paymentMethods',           V::LIST_VALIDATION ],
+        F::CURRENCIES_ACCEPTED        => [ 1, 1, 1, 1, 'currenciesAccepted',       V::LIST_VALIDATION ],
+        F::SPECIES_COMMENT            => [ 0, 1, 1, 1, 'speciesComment',           V::ANYTHING ],
+        F::SPECIES_DOES               => [ 1, 1, 1, 1, 'speciesDoes',              V::ANYTHING ],
+        F::SPECIES_DOESNT             => [ 1, 1, 1, 1, 'speciesDoesnt',            V::ANYTHING ],
+        F::URL_FURSUITREVIEW          => [ 0, 1, 1, 1, 'fursuitReviewUrl',         V::FSR_URL ],
+        F::URL_WEBSITE                => [ 0, 1, 1, 1, 'websiteUrl',               V::GENERIC_URL ],
+        F::URL_PRICES                 => [ 1, 1, 1, 1, 'pricesUrl',                V::GENERIC_URL_LIST ],
+        F::URL_COMMISSIONS            => [ 1, 1, 1, 1, 'commissionsUrl',           V::GENERIC_URL_LIST ],
+        F::URL_FAQ                    => [ 0, 1, 1, 1, 'faqUrl',                   V::GENERIC_URL ],
+        F::URL_FUR_AFFINITY           => [ 0, 1, 1, 1, 'furAffinityUrl',           V::FA_URL ],
+        F::URL_DEVIANTART             => [ 0, 1, 1, 1, 'deviantArtUrl',            V::DA_URL ],
+        F::URL_TWITTER                => [ 0, 1, 1, 1, 'twitterUrl',               V::TWITTER_URL ],
+        F::URL_FACEBOOK               => [ 0, 1, 1, 1, 'facebookUrl',              V::FACEBOOK_URL ],
+        F::URL_TUMBLR                 => [ 0, 1, 1, 1, 'tumblrUrl',                V::TUMBLR_URL ],
+        F::URL_INSTAGRAM              => [ 0, 1, 1, 1, 'instagramUrl',             V::INSTAGRAM_URL ],
+        F::URL_YOUTUBE                => [ 0, 1, 1, 1, 'youtubeUrl',               V::YOUTUBE_URL ],
+        F::URL_LINKLIST               => [ 0, 1, 1, 1, 'linklistUrl',              V::GENERIC_URL ],
+        F::URL_FURRY_AMINO            => [ 0, 1, 1, 1, 'furryAminoUrl',            V::GENERIC_URL ],
+        F::URL_ETSY                   => [ 0, 1, 1, 1, 'etsyUrl',                  V::GENERIC_URL ],
+        F::URL_THE_DEALERS_DEN        => [ 0, 1, 1, 1, 'theDealersDenUrl',         V::GENERIC_URL ],
+        F::URL_OTHER_SHOP             => [ 0, 1, 1, 1, 'otherShopUrl',             V::GENERIC_URL ],
+        F::URL_QUEUE                  => [ 0, 1, 1, 1, 'queueUrl',                 V::GENERIC_URL ],
+        F::URL_SCRITCH                => [ 0, 1, 1, 1, 'scritchUrl',               V::SCRITCH_URL ],
+        F::URL_FURTRACK               => [ 0, 1, 1, 1, 'furtrackUrl',              V::FURTRACK_URL ],
+        F::URL_PHOTOS                 => [ 1, 1, 1, 1, 'photoUrls',                V::PHOTO_URL_LIST ],
+        F::URL_MINIATURES             => [ 1, 1, 1, 1, 'miniatureUrls',            V::MINIATURE_URL_LIST ],
+        F::URL_OTHER                  => [ 0, 1, 1, 1, 'otherUrls',                V::ANYTHING ],
+        F::NOTES                      => [ 0, 1, 0, 1, 'notes',                    V::ANYTHING ],
+        F::INACTIVE_REASON            => [ 0, 1, 1, 1, 'inactiveReason',           V::ANYTHING ],
+        F::CS_LAST_CHECK              => [ 0, 0, 0, 1, 'csLastCheck',              V::ANYTHING ],
+        F::CS_TRACKER_ISSUE           => [ 0, 1, 0, 1, 'csTrackerIssue',           V::ANYTHING ],
+        F::BP_LAST_CHECK              => [ 0, 0, 0, 1, 'bpLastCheck',              V::ANYTHING ],
+        F::BP_TRACKER_ISSUE           => [ 0, 1, 0, 1, 'bpTrackerIssue',           V::ANYTHING ],
+        F::OPEN_FOR                   => [ 1, 0, 0, 1, 'openFor',                  V::ANYTHING ],
+        F::CLOSED_FOR                 => [ 1, 0, 0, 1, 'closedFor',                V::ANYTHING ],
+        F::COMPLETENESS               => [ 0, 0, 0, 1, 'completeness',             V::ANYTHING ],
+        F::CONTACT_ALLOWED            => [ 0, 1, 0, 1, 'contactAllowed',           V::ANYTHING ],
+        F::CONTACT_METHOD             => [ 0, 1, 0, 0, 'contactMethod',            V::ANYTHING ],
+        F::CONTACT_ADDRESS_PLAIN      => [ 0, 1, 0, 0, 'contactAddressPlain',      V::ANYTHING ],
+        F::CONTACT_INFO_ORIGINAL      => [ 0, 1, 0, 0, 'contactInfoOriginal',      V::ANYTHING ],
+        F::CONTACT_INFO_OBFUSCATED    => [ 0, 1, 0, 1, 'contactInfoObfuscated',    V::ANYTHING ],
+        F::PASSWORD                   => [ 0, 1, 0, 0, 'password',                 V::ANYTHING ],
     ];
 
     public const URLS = [
-        Fields::URL_FURSUITREVIEW,
-        Fields::URL_WEBSITE,
-        Fields::URL_PRICES,
-        Fields::URL_FAQ,
-        Fields::URL_FUR_AFFINITY,
-        Fields::URL_DEVIANTART,
-        Fields::URL_TWITTER,
-        Fields::URL_FACEBOOK,
-        Fields::URL_TUMBLR,
-        Fields::URL_INSTAGRAM,
-        Fields::URL_YOUTUBE,
-        Fields::URL_LINKLIST,
-        Fields::URL_FURRY_AMINO,
-        Fields::URL_ETSY,
-        Fields::URL_THE_DEALERS_DEN,
-        Fields::URL_OTHER_SHOP,
-        Fields::URL_QUEUE,
-        Fields::URL_SCRITCH,
-        Fields::URL_PHOTOS,
-        Fields::URL_MINIATURES,
-        Fields::URL_OTHER,
-        Fields::URL_COMMISSIONS,
+        F::URL_FURSUITREVIEW,
+        F::URL_WEBSITE,
+        F::URL_PRICES,
+        F::URL_FAQ,
+        F::URL_FUR_AFFINITY,
+        F::URL_DEVIANTART,
+        F::URL_TWITTER,
+        F::URL_FACEBOOK,
+        F::URL_TUMBLR,
+        F::URL_INSTAGRAM,
+        F::URL_YOUTUBE,
+        F::URL_LINKLIST,
+        F::URL_FURRY_AMINO,
+        F::URL_ETSY,
+        F::URL_THE_DEALERS_DEN,
+        F::URL_OTHER_SHOP,
+        F::URL_QUEUE,
+        F::URL_SCRITCH,
+        F::URL_PHOTOS,
+        F::URL_MINIATURES,
+        F::URL_OTHER,
+        F::URL_COMMISSIONS,
     ];
 
     public const NON_INSPECTED_URLS = [
-        Fields::URL_SCRITCH,
-        Fields::URL_PHOTOS,
-        Fields::URL_MINIATURES,
-        Fields::URL_OTHER,
+        F::URL_SCRITCH,
+        F::URL_PHOTOS,
+        F::URL_MINIATURES,
+        F::URL_OTHER,
     ];
 }
