e590935fd139a910f67e4182eb38cbbdfdbac459
Veelkoov <veelkoov@protonmail.com>
2021-08-21 00:42:56 +0200

    Fixed & improved Rector config

--- rector.php
+++ rector.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
 use Rector\Php71\Rector\FuncCall\CountOnNullRector;
+use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
 use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\PHPUnit\Rector\ClassMethod\AddDoesNotPerformAssertionToNonAssertingTestRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\SymfonySetList;
@@ -19,11 +21,14 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(SetList::PHP_74);
     $containerConfigurator->import(SetList::PHP_80);
     $containerConfigurator->import(SymfonySetList::SYMFONY_50);
-    $containerConfigurator->import(SymfonySetList::SYMFONY_50_TYPES);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_52);
     $containerConfigurator->import(SymfonySetList::SYMFONY_CODE_QUALITY);
+    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_60);
     $containerConfigurator->import(PHPUnitSetList::PHPUNIT_70);
     $containerConfigurator->import(PHPUnitSetList::PHPUNIT_75);
     $containerConfigurator->import(PHPUnitSetList::PHPUNIT_80);
+    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_90);
+    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_91);
 
     $parameters = $containerConfigurator->parameters();
 
@@ -31,6 +36,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         AddLiteralSeparatorToNumberRector::class,
         CountOnNullRector::class, // TODO: Reconsider
         ClassPropertyAssignToConstructorPromotionRector::class, // TODO: Remove when Doctrine annotations are supported in constructor parameters
+        AddDoesNotPerformAssertionToNonAssertingTestRector::class, // Not an issue in our case; we don't have "unsafe" tests
+        JsonThrowOnErrorRector::class, // Using our own utility class, using named arguments
     ]);
 
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
