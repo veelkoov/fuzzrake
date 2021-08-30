72752f57e1da3829397c5c29779cce3c20c40221
Veelkoov <veelkoov@protonmail.com>
2021-08-30 23:24:57 +0200

    Removed redundant PHPDoc

--- src/Doctrine/EntityManagerDecorator.php
+++ src/Doctrine/EntityManagerDecorator.php
@@ -6,16 +6,9 @@ namespace App\Doctrine;
 
 use App\Utils\Artisan\SmartAccessDecorator;
 use Doctrine\ORM\Decorator\EntityManagerDecorator as DoctrineEntityManagerDecorator;
-use Doctrine\ORM\ORMException;
-use Doctrine\ORM\ORMInvalidArgumentException;
 
 class EntityManagerDecorator extends DoctrineEntityManagerDecorator
 {
-    /**
-     * @throws ORMInvalidArgumentException
-     * @throws ORMException
-     * @noinspection PhpDocRedundantThrowsInspection ORMException is thrown by decorated class, not the interface
-     */
     public function persist($object)
     {
         if ($object instanceof SmartAccessDecorator) {
