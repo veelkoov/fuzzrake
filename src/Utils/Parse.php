7d2fa9c2229aa62c42a884e0fa662ab34fbbe8c9
Veelkoov <veelkoov@protonmail.com>
2021-09-01 21:25:25 +0200

    Improve comments&formatting, add support for parsing booleans

--- src/Utils/Parse.php
+++ src/Utils/Parse.php
@@ -6,6 +6,11 @@ namespace App\Utils;
 
 use App\Utils\Traits\UtilityClass;
 
+/**
+ * Methods prefixes:
+ *   t - trims input (if string provided as the argument),
+ *   n - returns null instead of throwing exception on failure.
+ */
 final class Parse
 {
     use UtilityClass;
@@ -79,5 +84,12 @@ final class Parse
         }
 
         return $result;
+    }
+
+    public static function nBool(string $value): ?bool
+    {
+        $value = '' === $value ? 'null' : $value; // Return null for empty string as well
+
+        return filter_var($value, FILTER_VALIDATE_BOOL, FILTER_NULL_ON_FAILURE);
     }
 }
