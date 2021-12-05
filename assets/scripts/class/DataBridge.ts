7437e8e1e389a2c0a0b5ce14e6be320b41d74620
Veelkoov <@veelkoov>
2021-12-05 12:06:00 +0100

    Use single data bridge global

--- assets/scripts/class/DataBridge.ts
+++ assets/scripts/class/DataBridge.ts
@@ -1,51 +1,46 @@
 import Artisan from "./Artisan";
 
-declare const DATA_UPDATES_URL: string;
-declare const TRACKING_URL: string;
-declare const IU_FORM_REDIRECT_URL: string;
-declare const REQUEST_FORM_URL: string;
-declare const TRACKING_FAILED_IMG_SRC: string;
+declare const DATA_BRIDGE: { [key: string]: string };
+
 declare const ARTISANS: Artisan[];
 declare const MAKER_IDS_MAP: object;
-declare const API_BASE_URL: string;
-declare const GOOGLE_RECAPTCHA_SITE_KEY: string;
 declare const SPECIES: object;
 
 export default abstract class DataBridge {
-    public static getMakerIdsMap(): object {
-        return MAKER_IDS_MAP;
+    public static getDataUpdatesUrl(): string {
+        return DATA_BRIDGE.dataUpdatesUrl;
+    }
+
+    public static getTrackingUrl(): string {
+        return DATA_BRIDGE.trackingUrl;
+    }
+
+    public static getIuFormRedirectUrl(): string {
+        return DATA_BRIDGE.iuFormRedirectUrl;
+    }
+
+    public static getRequestFormUrl(): string {
+        return DATA_BRIDGE.requestFormUrl;
+    }
+
+    public static getTrackingFailedImgSrc(): string {
+        return DATA_BRIDGE.trackingFailedImgSrc;
+    }
+
+    public static getApiUrl(path: string): string {
+        return DATA_BRIDGE.apiBaseUrl + path;
+    }
+
+    public static getGoogleRecaptchaSiteKey(): string {
+        return DATA_BRIDGE.googleRecaptchaSiteKey;
     }
 
     public static getArtisans(): Artisan[] {
         return ARTISANS;
     }
 
-    public static getDataUpdatesUrl(): string {
-        return DATA_UPDATES_URL;
-    }
-
-    public static getTrackingUrl(): string {
-        return TRACKING_URL;
-    }
-
-    public static getIuFormRedirectUrl(): string {
-        return IU_FORM_REDIRECT_URL;
-    }
-
-    public static getRequestFormUrl(): string {
-        return REQUEST_FORM_URL;
-    }
-
-    public static getTrackingFailedImgSrc(): string {
-        return TRACKING_FAILED_IMG_SRC;
-    }
-
-    public static getApiUrl(path: string): string {
-        return API_BASE_URL + path;
-    }
-
-    public static getGoogleRecaptchaSiteKey(): string {
-        return GOOGLE_RECAPTCHA_SITE_KEY;
+    public static getMakerIdsMap(): object {
+        return MAKER_IDS_MAP;
     }
 
     public static getSpecies(): object {
