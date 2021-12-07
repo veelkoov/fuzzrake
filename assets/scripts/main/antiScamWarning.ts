3272de42b4edf25ccf6b84925f1859a0f3b55148
Veelkoov <veelkoov@protonmail.com>
2021-12-07 23:02:55 +0100

    Remove leftovers after top-fixed menu

--- assets/scripts/main/antiScamWarning.ts
+++ assets/scripts/main/antiScamWarning.ts
@@ -4,8 +4,8 @@ function acknowledgedCallback(event: Event): void {
     jQuery('#scam-risk-warning, #scam-risk-acknowledged').toggle();
 
     // Anti-scam warning causes the user to be at the bottom of the table
-    let offset = jQuery('#data-table-container').offset() || { 'top': 70 };
-    window.scrollTo(0, offset.top - 70); // FIXME: 70!!!
+    let offset = jQuery('#data-table-container').offset() || { 'top': 0 };
+    window.scrollTo(0, offset.top - 5);
 }
 
 export function init(): (() => void)[] {
