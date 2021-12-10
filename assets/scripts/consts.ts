410becd83bd9a42e5518ea690a768ce235e8fa09
Veelkoov <veelkoov@protonmail.com>
2021-12-11 00:45:57 +0100

    Use const instead of let

--- assets/scripts/consts.ts
+++ assets/scripts/consts.ts
@@ -1,4 +1,4 @@
-export let makerIdRegexp = /^[A-Z0-9]{7}$/;
-export let makerIdHashRegexp = /^#[A-Z0-9]{7}$/;
+export const makerIdRegexp = /^[A-Z0-9]{7}$/;
+export const makerIdHashRegexp = /^#[A-Z0-9]{7}$/;
 
-export let NO_CONTACT_ALLOWED = 'NO'; // grep-no-contact-allowed
+export const NO_CONTACT_ALLOWED = 'NO'; // grep-no-contact-allowed
