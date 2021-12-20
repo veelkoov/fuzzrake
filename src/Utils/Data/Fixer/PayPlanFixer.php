de5a8f3d98db87c5d652e44a6db20f9a649b9984
Veelkoov <veelkoov@protonmail.com>
2021-12-20 20:50:14 +0100

    Fixer improvements

---
+++ src/Utils/Data/Fixer/PayPlanFixer.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Utils\Data\Fixer;
+
+use App\Utils\Regexp\Replacements;
+
+class PayPlanFixer extends AbstractStringFixer
+{
+    private readonly Replacements $replacements;
+
+    public function __construct(array $noPayPlans, array $strings)
+    {
+        parent::__construct($strings);
+
+        $this->replacements = new Replacements($noPayPlans['replacements'], 'i', $noPayPlans['commonRegexPrefix'], $noPayPlans['commonRegexSuffix']);
+    }
+
+    public function fix(string $fieldName, string $subject): string
+    {
+        $result = parent::fix($fieldName, $subject);
+
+        return $this->replacements->do($result);
+    }
+}
