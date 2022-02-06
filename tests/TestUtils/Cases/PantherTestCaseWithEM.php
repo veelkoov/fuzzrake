36c4ad39508d7a25cdc5939149d9c976c71bd97f
Veelkoov <veelkoov@protonmail.com>
2022-02-06 16:31:49 +0100

    Helper test methods

--- tests/TestUtils/Cases/PantherTestCaseWithEM.php
+++ tests/TestUtils/Cases/PantherTestCaseWithEM.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Tests\TestUtils\Cases;
 
 use App\Tests\TestUtils\Cases\Traits\EntityManagerTrait;
+use Facebook\WebDriver\Exception\NoSuchElementException;
+use Facebook\WebDriver\Exception\TimeoutException;
 use Facebook\WebDriver\WebDriver;
 use Facebook\WebDriver\WebDriverDimension;
 use Symfony\Component\Panther\Client as PantherClient;
@@ -40,5 +42,40 @@ abstract class PantherTestCaseWithEM extends PantherTestCase
         $line = $somekindoftrace[0]['line'];
 
         $client->takeScreenshot("var/screenshots/$class/$line.png");
+    }
+
+    /**
+     * @throws NoSuchElementException|TimeoutException
+     */
+    protected static function waitUntilShows(string $locator, int $millisecondsForAnimation = 200): void
+    {
+        usleep($millisecondsForAnimation * 1000);
+        self::$pantherClient->waitForVisibility($locator, 1);
+    }
+
+    protected static function assertVisible(string $locator): void
+    {
+        $exception = null;
+
+        try {
+            self::$pantherClient->waitForVisibility($locator, 1);
+        } catch (NoSuchElementException|TimeoutException $caught) {
+            $exception = $caught;
+        }
+
+        self::assertNull($exception, "Element '$locator' is not visible.");
+    }
+
+    protected static function assertInvisible(string $locator): void
+    {
+        $exception = null;
+
+        try {
+            self::$pantherClient->waitForInvisibility($locator, 1);
+        } catch (NoSuchElementException|TimeoutException $caught) {
+            $exception = $caught;
+        }
+
+        self::assertNull($exception, "Element '$locator' is visible.");
     }
 }
