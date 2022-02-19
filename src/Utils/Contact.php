b9ff8de5091dc741740c6efddb0391e513b33a09
Veelkoov <veelkoov@protonmail.com>
2022-02-19 19:57:20 +0100

    Support unsupported Instagram

--- src/Utils/Contact.php
+++ src/Utils/Contact.php
@@ -13,17 +13,19 @@ final class Contact
     use UtilityClass;
 
     final public const INVALID = 'INVALID';
+    final public const INSTAGRAM = 'INSTAGRAM';
     final public const TWITTER = 'TWITTER';
     final public const TELEGRAM = 'TELEGRAM';
     final public const E_MAIL = 'E-MAIL';
 
     private const PATTERNS = [
-        '(?:^|email: ?| |\()([a-z0-9._-]+@[a-z0-9.]+)(?:$|[ )])' => [self::E_MAIL,   ''],
-        'telegram *[:-]? ?[ @]([a-z0-9_]+)'                      => [self::TELEGRAM, '@'],
-        '@?([a-z0-9_]+) (?:on|-) (twitter or )?telegram'         => [self::TELEGRAM, '@'],
-        '@?([a-z0-9_]+)( on|@) twitter'                          => [self::TWITTER,  '@'],
-        '^https://twitter.com/([a-z0-9_-]+)$'                    => [self::TWITTER,  ''],
-        'twitter[-:, ]* ?@?([a-z0-9_]+)'                         => [self::TWITTER,  '@'],
+        '(?:^|email: ?| |\()([a-z0-9._-]+@[a-z0-9.]+)(?:$|[ )])' => [self::E_MAIL,    ''],
+        'telegram *[:-]? ?[ @]([a-z0-9_]+)'                      => [self::TELEGRAM,  '@'],
+        '@?([a-z0-9_]+) (?:on|-) (twitter or )?telegram'         => [self::TELEGRAM,  '@'],
+        '@?([a-z0-9_]+)( on|@) twitter'                          => [self::TWITTER,   '@'],
+        '^https://twitter.com/([a-z0-9_-]+)$'                    => [self::TWITTER,   ''],
+        'twitter[-:, ]* ?@?([a-z0-9_]+)'                         => [self::TWITTER,   '@'],
+        'instagram[-:, ]* ?@?([a-z0-9_]+)'                       => [self::INSTAGRAM, '@'],
     ];
 
     /**
