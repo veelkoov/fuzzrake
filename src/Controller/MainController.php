e95a5174a6559e290515fb6f6eb46711c685f89e
Veelkoov <veelkoov@protonmail.com>
2022-03-28 19:48:16 +0200

    Reschedule CST; reduce cache lifetime on main controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -26,57 +26,27 @@ class MainController extends AbstractController
      */
     #[Route(path: '/', name: RouteName::MAIN)]
     #[Route(path: '/index.html')]
-    #[Cache(public: true)]
+    #[Cache(maxage: 3600, public: true)]
     public function main(ArtisanRepository $artisanRepository, MakerIdRepository $makerIdRepository, FilterService $filterService, Species $species, StatisticsService $statistics): Response
     {
-        $response = $this->render('main/main.html.twig', [
+        return $this->render('main/main.html.twig', [
             'artisans'            => Artisan::wrapAll($artisanRepository->getAll()),
             'makerIdsMap'         => $makerIdRepository->getOldToNewMakerIdsMap(),
             'stats'               => $statistics->getMainPageStats(),
             'filters'             => $filterService->getFiltersTplData(),
             'species'             => $species->getTree(),
         ]);
-
-        self::setExpires($response);
-
-        return $response;
     }
 
     /**
      * @throws DateTimeException
      */
     #[Route(path: '/new.html', name: RouteName::NEW_ARTISANS)]
-    #[Cache(public: true)]
+    #[Cache(maxage: 3600, public: true)]
     public function newArtisans(ArtisanRepository $artisanRepository): Response
     {
-        $response = $this->render('main/new.html.twig', [
+        return $this->render('main/new.html.twig', [
             'artisans' => Artisan::wrapAll($artisanRepository->getNew()),
         ]);
-
-        self::setExpires($response);
-
-        return $response;
-    }
-
-    /**
-     * @throws DateTimeException
-     */
-    private static function setExpires(Response $response): void
-    {
-        $now = DateTimeUtils::getNowUtc();
-
-        $nextExpiresCandidates = [ // TODO: Move to configuration, make somehow connected to the Cron job; make safer assumptions (e.g. max 4-6h)? grep-tracking-frequency
-            DateTimeUtils::getUtcAt('6:10'),
-            DateTimeUtils::getUtcAt('18:10'),
-            DateTimeUtils::getUtcAt('tomorrow 6:10'),
-        ];
-
-        foreach ($nextExpiresCandidates as $nextExpires) {
-            if ($nextExpires > $now) {
-                $response->setExpires($nextExpires);
-
-                return;
-            }
-        }
     }
 }
