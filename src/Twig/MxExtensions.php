7b27cbd5eb08982e195b5865a34b7b79661f94ba
Veelkoov <veelkoov@protonmail.com>
2022-08-18 23:41:39 +0200

    Improved displaying differences

--- src/Twig/MxExtensions.php
+++ src/Twig/MxExtensions.php
@@ -7,9 +7,12 @@ namespace App\Twig;
 use App\DataDefinitions\Fields\Field;
 use App\Entity\Artisan as ArtisanE;
 use App\Utils\Artisan\SmartAccessDecorator as Artisan;
+use App\Utils\StringList;
 use App\Utils\StrUtils;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+
+use function Psl\Iter\contains;
 
 class MxExtensions extends AbstractExtension
 {
@@ -19,6 +22,11 @@ class MxExtensions extends AbstractExtension
             new TwigFilter('smart', fn (Artisan|ArtisanE $artisan) => $this->smartFilter($artisan)),
             new TwigFilter('as_str', fn (mixed $value) => $this->asStr($value)),
             new TwigFilter('as_field', fn (string $name) => $this->asField($name)),
+            new TwigFilter('difference',
+                function (Field $field, bool $isNew, Artisan $current, Artisan $other): string {
+                    return $this->difference($field, $isNew, $current, $other);
+                }, ['is_safe' => ['html']],
+            ),
         ];
     }
 
@@ -42,5 +50,30 @@ class MxExtensions extends AbstractExtension
     private function asField(string $name): Field
     {
         return Field::from($name);
+    }
+
+    private function difference(Field $field, bool $isNew, Artisan $subject, Artisan $other): string
+    {
+        if (!$field->isList()) {
+            $class = $isNew ? 'text-success' : 'text-danger';
+
+            return '<span class="'.$class.'">'.htmlspecialchars(StrUtils::asStr($subject->get($field))).'</span>';
+        }
+
+        $bsClass = $isNew ? 'bg-success' : 'bg-danger';
+
+        $result = '';
+
+        $subjectItems = StringList::unpack($subject->getString($field));
+        $otherItems = StringList::unpack($other->getString($field));
+
+        foreach ($subjectItems as $item) {
+            $itemClass = contains($otherItems, $item) ? 'bg-secondary' : $bsClass;
+            $text = htmlspecialchars($item);
+
+            $result .= " <span class=\"badge rounded-pill $itemClass\">$text</span> ";
+        }
+
+        return $result;
     }
 }
