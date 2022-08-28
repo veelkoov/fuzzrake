88f08dfa46a69877456bd7a9ba06049fb2591fff
Veelkoov <@veelkoov>
2022-08-28 18:12:19 +0200

    Dead code

--- assets/scripts/entry/events.ts
+++ assets/scripts/entry/events.ts
@@ -1,11 +1,1 @@
 import '../../styles/events.scss';
-
-jQuery(function (): void {
-    jQuery('#events-list .toggle-details').on('click', (event: Event) => {
-        event.preventDefault();
-
-        jQuery(event.target).parents('.events-item')
-            .find('.event-details')
-            .toggle();
-    });
-});
