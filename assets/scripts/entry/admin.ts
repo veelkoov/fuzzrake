750a0bda58545df28c46ee144dad3de80beda106
Veelkoov <veelkoov@protonmail.com>
2022-09-11 01:02:05 +0200

    Allow easy fixes from the UI

--- assets/scripts/entry/admin.ts
+++ assets/scripts/entry/admin.ts
@@ -1,1 +1,13 @@
 import '../../styles/admin.scss';
+
+jQuery(function () {
+    jQuery('button.fix-button').on('click', function (event) {
+        const directivesTextarea = jQuery('#submission_directives');
+
+        const $valueRow = jQuery(event.target).parents('#mx-submission tr');
+        const field = $valueRow.data('field');
+        const value = $valueRow.data('value');
+
+        directivesTextarea.val(`${directivesTextarea.val()}set ${field} "${value}"\n`);
+    });
+});
