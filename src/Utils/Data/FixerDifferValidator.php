dd6736db27e76ab1ce009ae120d3a7137b9c705a
Veelkoov <veelkoov@protonmail.com>
2022-09-11 15:44:33 +0200

    Removed unused FDV code

--- src/Utils/Data/FixerDifferValidator.php
+++ src/Utils/Data/FixerDifferValidator.php
@@ -4,13 +4,9 @@ declare(strict_types=1);
 
 namespace App\Utils\Data;
 
-use App\DataDefinitions\Fields\Field;
 use App\DataDefinitions\Fields\Fields;
 use App\DataDefinitions\Fields\FieldsList;
-use App\IuHandling\Import\Manager;
 use App\Utils\Artisan\SmartAccessDecorator as Artisan;
-use App\Utils\Console\Formatter;
-use App\Utils\StrUtils;
 use InvalidArgumentException;
 
 class FixerDifferValidator
@@ -18,8 +14,6 @@ class FixerDifferValidator
     final public const FIX = 1;
     final public const SHOW_DIFF = 2;
     final public const RESET_INVALID_PLUS_SHOW_FIX_CMD = 8;
-    final public const SHOW_FIX_CMD_FOR_INVALID = 16;
-    final public const USE_SET_FOR_FIX_CMD = 32;
 
     private readonly Differ $differ;
 
@@ -50,43 +44,10 @@ class FixerDifferValidator
             $isValid = $this->validator->isValid($artisan->getChanged(), $field);
             $resetAndShowFixCommand = $flags & self::RESET_INVALID_PLUS_SHOW_FIX_CMD && !$isValid;
 
-            if (!$isValid && $flags & self::SHOW_FIX_CMD_FOR_INVALID || $resetAndShowFixCommand) {
-                $this->printFixCommandOptionally($field, $artisan, (bool) ($flags & self::USE_SET_FOR_FIX_CMD));
-            }
-
             if ($resetAndShowFixCommand) {
                 $artisan->getChanged()->set($field, $artisan->getSubject()->get($field));
             }
         }
-    }
-
-    private function printFixCommandOptionally(Field $field, ArtisanChanges $artisan, bool $useSetForFixCmd): void
-    {
-        if (!$this->hideFixCommandFor($field)) {
-            $originalVal = StrUtils::strSafeForCli(StrUtils::asStr($artisan->getSubject()->get($field)));
-
-            if (!$this->validator->isValid($artisan->getChanged(), $field)) {
-                $originalVal = Formatter::invalid($originalVal);
-            }
-
-            $proposedVal = StrUtils::strSafeForCli(StrUtils::asStr($artisan->getChanged()->get($field))) ?: 'NEW_VALUE';
-
-            if ($useSetForFixCmd) {
-                $fixCmd = Manager::CMD_SET." $field->name \"$proposedVal\"";
-            } else {
-                $fixCmd = Manager::CMD_REPLACE." $field->name \"$originalVal\" \"$proposedVal\"";
-            }
-
-            $this->printer->writeln(Formatter::fix("    $fixCmd"));
-        }
-    }
-
-    private function hideFixCommandFor(Field $field): bool
-    {
-        return !$field->isInIuForm() || in_array($field, [
-            Field::CONTACT_ADDRESS_PLAIN,
-            Field::PASSWORD,
-        ]);
     }
 
     private function getArtisanFixWip(Artisan|ArtisanChanges $artisan): ArtisanChanges
