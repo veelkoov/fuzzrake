da9c32dbaab94002d0984389e772997f40fe79ff
Veelkoov <veelkoov@protonmail.com>
2023-04-15 23:31:13 +0200

    Cache species stats

--- src/Data/Stats/SpeciesStatsService.php
+++ src/Data/Stats/SpeciesStatsService.php
@@ -7,20 +7,25 @@ namespace App\Data\Stats;
 use App\Data\Species\SpeciesService;
 use App\Data\Stats\Compute\SpeciesCalculator;
 use App\Repository\ArtisanRepository;
+use App\Service\Cache;
 use App\Utils\Artisan\SmartAccessDecorator as Artisan;
+use App\ValueObject\CacheTags;
 
 class SpeciesStatsService
 {
     public function __construct(
         private readonly ArtisanRepository $artisanRepository,
         private readonly SpeciesService $speciesService,
+        private readonly Cache $cache,
     ) {
     }
 
     public function getStats(): SpeciesStats
     {
-        $artisans = Artisan::wrapAll($this->artisanRepository->getActive());
+        return $this->cache->getCached('SpeciesStatsService.getStats', CacheTags::ARTISANS, function () {
+            $artisans = Artisan::wrapAll($this->artisanRepository->getActive());
 
-        return SpeciesCalculator::for($this->speciesService->getCompleteList())->add($artisans)->get();
+            return SpeciesCalculator::for($this->speciesService->getCompleteList())->add($artisans)->get();
+        });
     }
 }
