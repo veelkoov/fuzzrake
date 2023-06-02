9f7529d861d6ea79eaf8683f660ce1f2ffb269a2
Veelkoov <veelkoov@protonmail.com>
2023-06-02 14:41:41 +0200

    Introduce logger

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ repositories {
 dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.0")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.0")
+    implementation("io.github.oshai:kotlin-logging-jvm:4.0.0-beta-29")
+    implementation("org.slf4j:slf4j-simple:2.0.7") // TODO: Use file logging
+    implementation("ch.qos.logback:logback-core:1.3.5") // TODO: Use file logging
 
     testImplementation(kotlin("test"))
 }
