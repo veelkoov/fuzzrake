a96d17ff27b70748bdbb87064c9a55af4f1a419e
Veelkoov <veelkoov@protonmail.com>
2023-06-06 19:52:11 +0200

    Fix non-thread-safe object use

--- src/main/kotlin/digests/SHA224.kt
+++ src/main/kotlin/digests/SHA224.kt
@@ -6,6 +6,6 @@ import java.util.*
 object SHA224 {
     private val digest = MessageDigest.getInstance("SHA-224")
 
-    fun of(input: String): ByteArray = digest.digest(input.toByteArray())
+    fun of(input: String): ByteArray = synchronized(this) { digest.digest(input.toByteArray()) }
     fun hexOf(input: String): String = HexFormat.of().formatHex(of(input))
 }
