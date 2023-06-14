8fbbba5b7d1dda91f426febee95669bd1d6bb6ef
Veelkoov <veelkoov@protonmail.com>
2023-06-14 20:36:14 +0200

    Removed rewritten url fixing

--- src/Tracking/Web/WebsiteInfo.php
+++ src/Tracking/Web/WebsiteInfo.php
@@ -4,12 +4,6 @@ declare(strict_types=1);
 
 namespace App\Tracking\Web;
 
-use App\Tracking\Web\WebpageSnapshot\Snapshot;
-use App\Utils\UnbelievableRuntimeException;
-use TRegx\CleanRegex\Exception\NonexistentGroupException;
-use TRegx\CleanRegex\Match\Detail;
-use TRegx\CleanRegex\Pattern;
-
 use function Psl\Str\contains_ci;
 
 class WebsiteInfo
@@ -19,73 +13,11 @@ class WebsiteInfo
     private const FA_USER_NOT_FOUND_CONTENTS_SEARCH_STRING = 'This user cannot be found.';
     private const FA_SYSTEM_ERROR_CONTENTS_SEARCH_STRING = '<title>System Error</title>';
 
-    private const TRELLO_BOARD_URL_REGEXP = '^https?://trello.com/b/(?<boardId>[a-zA-Z0-9]+)/';
-
     private readonly Detector $detector;
-    private readonly Pattern $trelloBoardUrlPattern;
 
     public function __construct()
     {
         $this->detector = new Detector();
-
-        $this->trelloBoardUrlPattern = pattern(self::TRELLO_BOARD_URL_REGEXP, 'n');
-    }
-
-    private static function getTrelloBoardDataUrl(string $boardId): string
-    {
-        return "https://trello.com/1/Boards/$boardId?lists=open&list_fields=name&cards=visible&card_attachments=false&card_stickers=false&card_fields=desc%2CdescData%2Cname&card_checklists=none&members=none&member_fields=none&membersInvited=none&membersInvited_fields=none&memberships_orgMemberType=false&checklists=none&organization=false&organization_fields=none%2CdisplayName%2Cdesc%2CdescData%2Cwebsite&organization_tags=false&myPrefs=false&fields=name%2Cdesc%2CdescData";
-    }
-
-    /**
-     * @return string[]
-     */
-    public function getChildrenUrls(Snapshot $webpageSnapshot): array
-    {
-        if ($this->detector->isWixsite($webpageSnapshot)) {
-            return $this->getWixsiteDependencyUrls($webpageSnapshot);
-        } elseif ($this->detector->isTrello($webpageSnapshot)) {
-            return $this->getTrelloDependencyUrls($webpageSnapshot);
-        } elseif ($this->detector->isInstagram($webpageSnapshot)) {
-            return $this->getInstagramDependencyUrls($webpageSnapshot);
-        } else {
-            return [];
-        }
-    }
-
-    /**
-     * @return string[]
-     */
-    private function getWixsiteDependencyUrls(Snapshot $webpageSnapshot): array
-    {
-        // There used to be links '<link[^>]* href="(?<dataUrl>https://static.wixstatic.com/sites/[a-z0-9_]+\.json\.z\?v=\d+)"[^>]*>' containing data, not all's in HTML.
-        return [];
-    }
-
-    /**
-     * @return string[]
-     */
-    private function getTrelloDependencyUrls(Snapshot $webpageSnapshot): array
-    {
-        return $this->trelloBoardUrlPattern
-            ->match($webpageSnapshot->url)
-            ->findFirst()
-            ->map(function (Detail $detail): array {
-                try {
-                    return [self::getTrelloBoardDataUrl($detail->get('boardId'))];
-                } catch (NonexistentGroupException $e) { // @codeCoverageIgnoreStart
-                    throw new UnbelievableRuntimeException($e);
-                } // @codeCoverageIgnoreEnd
-            })
-            ->orReturn([]);
-    }
-
-    /**
-     * @return string[]
-     */
-    private function getInstagramDependencyUrls(Snapshot $webpageSnapshot): array
-    {
-        // Instead of downloading a "child", we now coerce Instagram URL.
-        return [];
     }
 
     public function getLatentCode(string $url, string $content): ?int
