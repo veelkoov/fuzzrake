7fad14bc3918deac6c66c73af7da77bb1efebb3c
Veelkoov <veelkoov@protonmail.com>
2023-08-06 14:06:21 +0200

    Rewritten parts of the backend in a programming language (#194)

--- src/Tracking/Web/HttpClient/HttpClient.php
+++ src/Tracking/Web/HttpClient/HttpClient.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Tracking\Web\HttpClient;
 
-use App\Tracking\Web\Detector;
 use Symfony\Component\BrowserKit\CookieJar;
 use Symfony\Component\BrowserKit\HttpBrowser;
 use Symfony\Component\HttpClient\CurlHttpClient;
@@ -16,11 +15,9 @@ class HttpClient
     private const CONNECTION_TIMEOUT_SEC = 10;
     private const TIMEOUT_SEC = 30;
 
-    private const USER_AGENT_DEFAULT = 'Mozilla/5.0 (compatible; GetFursuitBot/0.9; Symfony HttpClient/Curl; +https://getfursu.it/)';
-    private const USER_AGENT_FOR_INSTAGRAM = 'Mozilla/5.0 (Linux; Android 8.1.0; motorola one Build/OPKS28.63-18-3; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36 Instagram 72.0.0.21.98 Android (27/8.1.0; 320dpi; 720x1362; motorola; motorola one; deen_sprout; qcom; pt_BR; 132081645)'; // Thank you https://github.com/postaddictme/instagram-php-scraper/pull/545
+    private const USER_AGENT = 'Mozilla/5.0 (compatible; GetFursuitBot/0.9; Symfony HttpClient/Curl; +https://getfursu.it/)';
 
     private readonly CurlHttpClient $client;
-    private readonly Detector $detector;
 
     public function __construct()
     {
@@ -29,8 +26,6 @@ class HttpClient
             'timeout'      => self::CONNECTION_TIMEOUT_SEC,
             'max_duration' => self::TIMEOUT_SEC,
         ]);
-
-        $this->detector = new Detector();
     }
 
     /**
@@ -40,7 +35,7 @@ class HttpClient
      */
     public function get(string $url, CookieJar $cookieJar = null, array $additionalHeaders = []): ResponseInterface
     {
-        $additionalHeaders['User-Agent'] = $this->getUserAgent($url);
+        $additionalHeaders['User-Agent'] = self::USER_AGENT;
 
         $options = [
             'headers' => $this->appendCookieToHeaders($additionalHeaders, $cookieJar, $url),
@@ -84,12 +79,5 @@ class HttpClient
         }
 
         return $headers;
-    }
-
-    private function getUserAgent(string $url): string
-    {
-        return $this->detector->isInstagram($url)
-            ? self::USER_AGENT_FOR_INSTAGRAM
-            : self::USER_AGENT_DEFAULT;
     }
 }
--- src/Tracking/Web/Url/UrlUtils.php
+++ src/Tracking/Web/Url/UrlUtils.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Tracking\Web\Url;
 
-use App\Utils\Regexp\Patterns;
 use App\Utils\Traits\UtilityClass;
 
 use function Psl\Str\Byte\strip_prefix;
@@ -16,13 +15,5 @@ final class UrlUtils
     public static function hostFromUrl(string $url): string
     {
         return strip_prefix(parse_url($url, PHP_URL_HOST) ?: 'invalid_host', 'www.');
-    }
-
-    public static function safeFileNameFromUrl(string $url): string
-    {
-        $result = Patterns::getI('^https?://(www\.)?|(\?|#).+$')->prune($url);
-        $result = Patterns::getI('[^a-z0-9_.-]+')->replace($result)->with('_');
-
-        return trim($result, '_');
     }
 }
