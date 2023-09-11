2a5057f754cfcab4c3ea7e788ff9d6f3e9c0f601
Veelkoov <veelkoov@protonmail.com>
2023-09-11 18:32:06 +0200

    Fixed EventsControllerWithEMTest.php

--- tests/Controller/EventsControllerWithEMTest.php
+++ tests/Controller/EventsControllerWithEMTest.php
@@ -44,44 +44,51 @@ class EventsControllerWithEMTest extends WebTestCaseWithEM
     /** @noinspection HtmlUnknownTarget */
     public function eventDescriptionDataProvider(): DataProvider
     {
-        return DataProvider::list(
-            (new Event())
-                ->setArtisanName('Artisan name 1')
-                ->setCheckedUrls('https://getfursu.it/page1.html')
-                ->setType(Event::TYPE_CS_UPDATED)
-                ->setNowOpenFor("Commissions\nPre-mades")
-                ->setNoLongerOpenFor('Artistic liberty')
-                ->setTrackingIssues(true), '
+        return DataProvider::tuples(
+            [
+                (new Event())
+                    ->setArtisanName('Artisan name 1')
+                    ->setCheckedUrls('https://getfursu.it/page1.html')
+                    ->setType(Event::TYPE_CS_UPDATED)
+                    ->setNowOpenFor("Commissions\nPre-mades")
+                    ->setNoLongerOpenFor('Artistic liberty')
+                    ->setTrackingIssues(true), '
                 <strong>Artisan name 1</strong> commissions status changed.
                 No longer open for: Artistic liberty.
                 <strong>Now open for: Commissions, Pre-mades.</strong>
                 Encountered apparent difficulties during status analysis.
                 Checked contents of:
                 <a href="https://getfursu.it/page1.html" target="_blank">getfursu.it/page1.html</a>.',
-            (new Event())
-                ->setArtisanName('Artisan name 2')
-                ->setCheckedUrls("https://getfursu.it/page2.html\nhttps://another.page/")
-                ->setType(Event::TYPE_CS_UPDATED)
-                ->setNowOpenFor('')
-                ->setNoLongerOpenFor('Pancakes')
-                ->setTrackingIssues(false), '
+            ],
+            [
+                (new Event())
+                    ->setArtisanName('Artisan name 2')
+                    ->setCheckedUrls("https://getfursu.it/page2.html\nhttps://another.page/")
+                    ->setType(Event::TYPE_CS_UPDATED)
+                    ->setNowOpenFor('')
+                    ->setNoLongerOpenFor('Pancakes')
+                    ->setTrackingIssues(false), '
                 <strong>Artisan name 2</strong> commissions status changed.
                 No longer open for: Pancakes.
                 Checked contents of:
                 <a href="https://getfursu.it/page2.html" target="_blank">getfursu.it/page2.html</a>,
                 <a href="https://another.page/" target="_blank">another.page</a>.',
-            (new Event())
-                ->setArtisanName('One more artisan')
-                ->setCheckedUrls('http://just-one-website/doc.php')
-                ->setType(Event::TYPE_CS_UPDATED)
-                ->setNowOpenFor("Carrots\nApples")
-                ->setNoLongerOpenFor('')
-                ->setTrackingIssues(true), '
+            ],
+            [
+                (new Event())
+                    ->setArtisanName('One more artisan')
+                    ->setCheckedUrls('http://just-one-website/doc.php')
+                    ->setType(Event::TYPE_CS_UPDATED)
+                    ->setNowOpenFor("Carrots\nApples")
+                    ->setNoLongerOpenFor('')
+                    ->setTrackingIssues(true), '
                 <strong>One more artisan</strong> commissions status changed.
                 <strong>Now open for: Carrots, Apples.</strong>
                 Encountered apparent difficulties during status analysis.
                 Checked contents of:
-                <a href="http://just-one-website/doc.php" target="_blank">just-one-website/doc.php</a>.');
+                <a href="http://just-one-website/doc.php" target="_blank">just-one-website/doc.php</a>.',
+            ],
+        );
     }
 
     public function testAtomFeedLoadsWithoutAnyEvents(): void
