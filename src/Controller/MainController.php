c9ef398a5959af05ea341f4aa8fdb4030f0f91fe
Veelkoov <veelkoov@protonmail.com>
2024-01-04 22:20:30 +0100

    Pause

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,14 +4,9 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Filtering\FiltersData\FiltersService;
 use App\Repository\ArtisanRepository;
-use App\Service\Cache as CacheService;
-use App\Service\DataService;
 use App\Utils\Artisan\SmartAccessDecorator as Artisan;
-use App\ValueObject\CacheTags;
 use App\ValueObject\Routing\RouteName;
-use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\Cache;
@@ -19,20 +14,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    /**
-     * @throws InvalidArgumentException
-     */
     #[Route(path: '/', name: RouteName::MAIN)]
     #[Cache(maxage: 3600, public: true)]
-    public function main(FiltersService $filterService, DataService $dataService, CacheService $cache): Response
+    public function main(): Response
     {
-        $filters = $cache->getCached('mainpage.filters', CacheTags::ARTISANS,
-            fn () => $filterService->getFiltersTplData());
-
-        return $this->render('main/main.html.twig', [
-            'stats'   => $dataService->getMainPageStats(),
-            'filters' => $filters,
-        ]);
+        return $this->render('pages/suspended.html.twig');
     }
 
     #[Route(path: '/new', name: RouteName::NEW_ARTISANS)]
