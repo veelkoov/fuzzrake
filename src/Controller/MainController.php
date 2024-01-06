95c548a724378dcf69fdf90582faa5913e68ca43
Veelkoov <veelkoov@protonmail.com>
2024-01-06 20:33:05 +0100

    Revert "Pause"
    This reverts commit c9ef398a5959af05ea341f4aa8fdb4030f0f91fe.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Filtering\FiltersData\FiltersService;
 use App\Repository\ArtisanRepository;
+use App\Service\Cache as CacheService;
+use App\Service\DataService;
 use App\Utils\Artisan\SmartAccessDecorator as Artisan;
+use App\ValueObject\CacheTags;
 use App\ValueObject\Routing\RouteName;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\Cache;
@@ -14,11 +19,20 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
+    /**
+     * @throws InvalidArgumentException
+     */
     #[Route(path: '/', name: RouteName::MAIN)]
     #[Cache(maxage: 3600, public: true)]
-    public function main(): Response
+    public function main(FiltersService $filterService, DataService $dataService, CacheService $cache): Response
     {
-        return $this->render('pages/suspended.html.twig');
+        $filters = $cache->getCached('mainpage.filters', CacheTags::ARTISANS,
+            fn () => $filterService->getFiltersTplData());
+
+        return $this->render('main/main.html.twig', [
+            'stats'   => $dataService->getMainPageStats(),
+            'filters' => $filters,
+        ]);
     }
 
     #[Route(path: '/new', name: RouteName::NEW_ARTISANS)]
