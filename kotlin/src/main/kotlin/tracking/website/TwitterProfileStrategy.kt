a358dd82db760ef6143c1057d2e7ebbce7024208
Veelkoov <veelkoov@protonmail.com>
2024-06-14 20:52:58 +0200

    Updated Twitter handling

--- kotlin/src/main/kotlin/tracking/website/TwitterProfileStrategy.kt
+++ kotlin/src/main/kotlin/tracking/website/TwitterProfileStrategy.kt
@@ -1,15 +1,16 @@
 package tracking.website
 
-import data.JsonException
-import data.JsonNavigator
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 import web.url.FreeUrl
 import web.url.Url
 
 private val logger = KotlinLogging.logger {}
 
+/**
+ * As of 2024-06-14 the Twitter's behavior is to send around 3700 bytes of the page,
+ * often enough to cover the description and title.
+ */
 object TwitterProfileStrategy : Strategy {
     private val cookieInitUrl = FreeUrl("https://twitter.com/", StandardStrategy)
     private val loginLocationHeader = Regex("^location: \\S+login\\S+$", setOf(RegexOption.MULTILINE, RegexOption.IGNORE_CASE))
@@ -20,47 +21,19 @@ object TwitterProfileStrategy : Strategy {
     override fun filterContents(input: String): String {
         val document = Jsoup.parse(input)
 
-        return tryFromLdJsonProfilePage(document) ?: tryFromMeta(document) ?: input
-    }
-
-    private fun tryFromMeta(document: Document): String? {
         val ogDescriptionNodes = document.head().selectXpath("//meta[@property='og:description']") // grep-code-og-description-removal
         val ogTitleNodes = document.head().selectXpath("//meta[@property='og:title']")
 
-        val ogTitle: String? = ogTitleNodes.attr("content")
-        val ogDescription: String? = ogDescriptionNodes.attr("content")
-
-        if (ogTitle == null || ogDescription == null) {
+        if (ogDescriptionNodes.isEmpty() || ogTitleNodes.isEmpty()) {
             logger.warn { "Failed reading og:title and/or og:description nodes" }
 
-            return null
+            return input
         }
+
+        val ogTitle: String = ogTitleNodes.attr("content")
+        val ogDescription: String = ogDescriptionNodes.attr("content")
 
         return ogTitle + "\n" + ogDescription
-    }
-
-    private fun tryFromLdJsonProfilePage(document: Document): String? {
-        val ldJsonNodes = document
-            .head()
-            .selectXpath("//script[@type='application/ld+json'][contains(text(), 'ProfilePage')]")
-
-        if (ldJsonNodes.isEmpty()) {
-            logger.info { "Failed to XPath Twitter ProfilePage schema script node" }
-
-            return null
-        }
-
-        return try {
-            val ldJsonData = JsonNavigator(ldJsonNodes.html())
-
-            ldJsonData.getString("author/givenName") +
-                    "\n" + ldJsonData.getString("author/description") +
-                    "\n" + ldJsonData.getString("author/homeLocation/name")
-        } catch (exception: JsonException) {
-            logger.warn(exception) { "Failed reading ProfilePage schema JSON" }
-
-            null
-        }
     }
 
     override fun getLatentCode(url: Url, contents: String, originalCode: Int): Int {
--- kotlin/src/test/kotlin/tracking/website/TwitterProfileStrategyTest.kt
+++ kotlin/src/test/kotlin/tracking/website/TwitterProfileStrategyTest.kt
@@ -1,6 +1,5 @@
 package tracking.website
 
-import data.Resource
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -8,26 +7,19 @@ class TwitterProfileStrategyTest {
     private val subject = TwitterProfileStrategy
 
     @Test
-    fun `Real-life, working scenario`() {
-        val result = subject.filterContents(Resource.read("/tracking/twitter_getfursuit_contents.txt"))
-
-        assertEquals("getfursu.it 🥌\nJOIN: https://t.co/3g0nZbnhqL 👀\n\nSingle-wolf (tired) powered service running on pancakes.\n\n🎶 🎤 🦖 🎸 🎹 🥁 🤘 🎶\nBeauharnois (the webserver)", result)
-    }
-
-    @Test
     fun `Simplified, working scenario`() {
-        val input = "<html><head><script type=\"application/ld+json\">{\"@type\":\"ProfilePage\",\"author\":{\"description\":\"Expected description\",\"givenName\":\"Expected name\",\"homeLocation\":{\"name\":\"Expected location\"}}}</script></head></html>"
+        val input = "<html><head><meta content=\"TheTitle\" property=\"og:title\"/><meta content=\"TheDescription\" property=\"og:description\"/></head></html>"
         val result = subject.filterContents(input)
 
-        assertEquals("Expected name\nExpected description\nExpected location", result)
+        assertEquals("TheTitle\nTheDescription", result)
     }
 
     @Test
-    fun `Empty description`() {
-        val input = "<html><head><script type=\"application/ld+json\">{\"@type\":\"ProfilePage\",\"author\":{\"description\":\"\",\"givenName\":\"\",\"homeLocation\":{\"name\":\"\"}}}</script></head></html>"
+    fun `Empty information`() {
+        val input = "<html><head><meta content=\"\" property=\"og:title\"/><meta content=\"\" property=\"og:description\"/></head></html>"
         val result = subject.filterContents(input)
 
-        assertEquals("\n\n", result)
+        assertEquals("\n", result)
     }
 
     @Test
@@ -39,16 +31,8 @@ class TwitterProfileStrategyTest {
     }
 
     @Test
-    fun `Missing script ld+json meta element`() {
-        val input = "<html><head></head></html>"
-        val result = subject.filterContents(input)
-
-        assertEquals(input, result)
-    }
-
-    @Test
-    fun `Missing expected ld+json field`() {
-        val input = "<html><head><script type=\"application/ld+json\">{\"@type\":\"ProfilePage\",\"author\":{\"description\":\"Description\",\"givenName\":\"Name\",\"homeLocation\":{\"wrongfieldname\":\"Location\"}}}</script></head></html>"
+    fun `Missing expected head element`() {
+        val input = "<html><head><meta content=\"TheDescription\" property=\"og:description\"/></head></html>"
         val result = subject.filterContents(input)
 
         assertEquals(input, result)
