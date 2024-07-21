2507460b702f975a6bf33c78716bc74d01b5f6a3
Veelkoov <veelkoov@protonmail.com>
2024-07-21 11:40:00 +0200

    Unused consts

--- kotlin/src/main/kotlin/data/KotlinDataManager.kt
+++ kotlin/src/main/kotlin/data/KotlinDataManager.kt
@@ -6,11 +6,6 @@ import kotlinx.serialization.json.Json
 
 object KotlinDataManager {
     const val SPECIES_FILTER = "species-filter"
-    const val PRODUCTION_MODELS_FILTER = "production-models-filter"
-    const val STYLES_FILTER = "styles-filter"
-    const val ORDER_TYPES_FILTER = "order-types-filter"
-    const val FEATURES_FILTER = "features-filter"
-    const val LANGUAGES_FILTER = "languages-filter"
 
     inline fun <reified T>set(name: String, data: T) {
         KotlinDataRepository.replaceData(name, Json.encodeToString(data))
