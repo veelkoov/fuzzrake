1948eedeef8ab1e66ba90582c6c0039f0901ddc6
Veelkoov <veelkoov@protonmail.com>
2024-08-11 21:24:22 +0200

    More cleanup

--- symfony/assets/scripts/Static.ts
+++ symfony/assets/scripts/Static.ts
@@ -7,29 +7,7 @@ export type Items = Array<Item>;
 
 export type FilterOptions = { 'items': Items, 'specialItems': SpecialItems };
 
-export type FiltersOptions = {
-    'orderTypes': FilterOptions,
-    'styles': FilterOptions,
-    'paymentPlans': FilterOptions,
-    'features': FilterOptions,
-    'productionModels': FilterOptions,
-    'openFor': FilterOptions,
-    'languages': FilterOptions,
-    'countries': FilterOptions,
-    'states': FilterOptions,
-    'species': FilterOptions,
-    'inactive': FilterOptions,
-};
-
 export default abstract class Static {
-    public static getMainPath(): string {
-        return DATA_BRIDGE.mainPath;
-    }
-
-    public static getTrackingPath(): string {
-        return DATA_BRIDGE.trackingPath;
-    }
-
     public static getApiUrl(path: string): string {
         return DATA_BRIDGE.apiBaseUrl + path;
     }
