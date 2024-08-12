b690075b1bbb05e0176da7ddb9547d75d6f026c1
Veelkoov <veelkoov@protonmail.com>
2024-08-12 21:33:41 +0200

    Removed obsolete types

--- symfony/assets/scripts/Static.ts
+++ symfony/assets/scripts/Static.ts
@@ -1,12 +1,5 @@
 declare const DATA_BRIDGE: { [key: string]: string };
 
-export type SpecialItem = { 'value': string, 'label': string, 'count': number, 'faIcon': string };
-export type SpecialItems = Array<SpecialItem>;
-export type Item = { 'value': string, 'label': string, 'count': number, 'subitems': Items };
-export type Items = Array<Item>;
-
-export type FilterOptions = { 'items': Items, 'specialItems': SpecialItems };
-
 export default abstract class Static {
     public static getApiUrl(path: string): string {
         return DATA_BRIDGE.apiBaseUrl + path;
