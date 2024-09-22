3c65332d2704c99a1b1f77fcd5c3f0463ccf28e9
Veelkoov <veelkoov@protonmail.com>
2024-09-22 12:01:15 +0200

    Remove the IS_MINOR field completely

--- symfony/tests/Utils/Artisan/SmartAccessDecoratorWithEMTest.php
+++ symfony/tests/Utils/Artisan/SmartAccessDecoratorWithEMTest.php
@@ -13,43 +13,55 @@ use App\Utils\Artisan\SmartAccessDecorator as Artisan;
  */
 class SmartAccessDecoratorWithEMTest extends KernelTestCaseWithEM
 {
+    /**
+     * This test assures that boolean field values are properly persisted. This is more like an end-to-end test,
+     * because it validates stuff on the ORM entity level.
+     */
     public function testBooleanValues(): void
     {
         self::bootKernel();
         $repo = self::getEM()->getRepository(ArtisanValue::class);
 
         $artisan = new Artisan();
-        $artisan->setIsMinor(null);
-        $artisan->setWorksWithMinors(null);
         self::persistAndFlush($artisan);
 
         $all = $repo->findAll();
-        self::assertCount(0, $all);
+        self::assertCount(0, $all, 'At this point there should be no value entity.');
 
-        $artisan->setIsMinor(true);
+        $artisan->setNsfwWebsite(true);
         self::flush();
 
         $all = $repo->findAll();
-        self::assertCount(1, $all);
-        self::assertEquals('IS_MINOR', $all[0]->getFieldName());
-        self::assertEquals('True', $all[0]->getValue());
-        $first = $all[0];
+        self::assertCount(1, $all,
+            "There should be a single value entity, the one we've just created.");
+        $nsfwWebsiteEntity = $all[0];
 
-        $artisan->setWorksWithMinors(false);
-        $artisan->setIsMinor(null);
+        self::assertEquals('NSFW_WEBSITE', $nsfwWebsiteEntity->getFieldName(),
+            'At this point we should have a single NSFW_WEBSITE value entity.');
+        self::assertEquals('True', $nsfwWebsiteEntity->getValue(),
+            'The NSFW_WEBSITE value entity should have "True" value.');
+
+        $artisan->setNsfwWebsite(null);
+        $artisan->setNsfwSocial(false);
         self::flush();
 
         $all = $repo->findAll();
-        self::assertCount(1, $all);
-        self::assertEquals('WORKS_WITH_MINORS', $all[0]->getFieldName());
-        self::assertEquals('False', $all[0]->getValue());
-        self::assertNotEquals($first->getId(), $all[0]->getId());
-        self::assertNotSame($first, $all[0]);
+        self::assertCount(1, $all,
+            'There should be still only one entity, the old one removed, but a new one introduced.');
+        $nsfwSocialEntity = $all[0];
 
-        $artisan->setWorksWithMinors(null);
+        self::assertEquals('NSFW_SOCIAL', $nsfwSocialEntity->getFieldName(),
+            'At this point we should have a single NSFW_SOCIAL value entity.');
+        self::assertEquals('False', $nsfwSocialEntity->getValue(),
+            'NSFW_SOCIAL value entity should have "True" value.');
+
+        self::assertNotEquals($nsfwWebsiteEntity->getId(), $nsfwSocialEntity->getId(),
+            'Different entities should be used to hold different fields.');
+
+        $artisan->setNsfwSocial(null);
         self::flush();
 
         $all = $repo->findAll();
-        self::assertCount(0, $all);
+        self::assertCount(0, $all, 'All value entitles should have been removed.');
     }
 }
