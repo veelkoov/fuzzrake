8288327c0c61e1f7ce6be1b840cbea4dc12d67f7
Veelkoov <@veelkoov>
2024-12-26 11:14:10 +0100

    Fix paths for Kotlin in container

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -15,8 +15,7 @@ mkdir -p -m 700 ./var/sessions
 mkdir -p -m 700 /tmp/phpstan
 
 for TARGET in \
-        /var/www/html/var \
-        /composer \
+        /var/www/html/symfony/var \
         /tmp/phpstan \
 ; do
     setfacl  -R -m u:www-data:rwX -m u:"$DOCKER_UID":rwX "$TARGET"
