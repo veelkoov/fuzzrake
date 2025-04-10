1ef44622bc169ae3b721c9691f3cb30b60d2372f
Veelkoov <veelkoov@protonmail.com>
2025-04-10 21:13:16 +0200

    Migrations bundle

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
