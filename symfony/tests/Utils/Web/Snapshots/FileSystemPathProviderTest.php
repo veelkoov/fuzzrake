fef790c1763ea60b4d8a67e7440fcae06265be09
Veelkoov <veelkoov@protonmail.com>
2025-06-13 22:59:21 +0200

    Translated FS path provider

---
+++ symfony/tests/Utils/Web/Snapshots/FileSystemPathProviderTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Utils\Web\Snapshots;
+
+use App\Utils\Web\Snapshots\FileSystemPathProvider;
+use App\Utils\Web\Url\FreeUrl;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Small;
+use PHPUnit\Framework\TestCase;
+
+#[Small]
+class FileSystemPathProviderTest extends TestCase
+{
+    /**
+     * @return list<array{string, string}>
+     */
+    public static function getSnapshotDirPathDataProvider(): array
+    {
+        return [
+            ['https://www.tumblr.com/getfursuit', 'T/tumblr.com/getfursuit'],
+            ['https://furries.club/@getfursuit', 'F/furries.club/getfursuit'],
+            ['https://github.com/veelkoov/fuzzrake/pull/194', 'G/github.com/veelkoov_fuzzrake_pull_194'],
+            ['https://getfursu.it/data_updates.html#anchor', 'G/getfursu.it/data_updates.html'],
+        ];
+    }
+
+    #[DataProvider('getSnapshotDirPathDataProvider')]
+    public function testGetSnapshotDirPath(string $inputUrl, string $expectedPrefix): void
+    {
+        $subject = new FileSystemPathProvider();
+
+        self::assertStringStartsWith($expectedPrefix, $subject->getSnapshotDirPath(new FreeUrl($inputUrl)));
+    }
+}
