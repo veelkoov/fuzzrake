b5c99178fe120eadb42affa204b2861f9e513071
Veelkoov <veelkoov@protonmail.com>
2025-06-14 14:16:29 +0200

    Removed unused stuff

--- kotlin/src/main/kotlin/data/JsonException.kt
+++
@@ -1,8 +0,0 @@
-package data
-
-import java.lang.Exception
-
-class JsonException : Exception {
-    constructor(message: String, cause: Throwable) : super(message, cause)
-    constructor(message: String) : super(message)
-}
