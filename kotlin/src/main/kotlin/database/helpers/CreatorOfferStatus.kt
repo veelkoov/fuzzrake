ebe439234266b0816fdaea9db1dfe6867e2f157f
Veelkoov <veelkoov@protonmail.com>
2025-07-20 10:34:11 +0200

    Remove obsolete/translated stuff

--- kotlin/src/main/kotlin/database/helpers/CreatorOfferStatus.kt
+++
@@ -1,7 +0,0 @@
-package database.helpers
-
-import database.entities.CreatorOfferStatus
-import tracking.statuses.OfferStatus
-import tracking.statuses.Status
-
-fun CreatorOfferStatus.toOfferStatus() = OfferStatus(offer, Status.fromBoolean(isOpen))
--- kotlin/src/main/kotlin/database/tables/CreatorIds.kt
+++
@@ -1,8 +0,0 @@
-package database.tables
-
-import org.jetbrains.exposed.dao.id.IntIdTable
-
-object CreatorIds : IntIdTable("creator_ids") {
-    val creator = reference("owner_creator_id", Creators)
-    val creatorId = varchar("creator_id", 7)
-}
