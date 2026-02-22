ee19b627e0d7ec3dbd63e1f03a48720bbeb44aa1
Veelkoov <veelkoov@protonmail.com>
2026-02-22 15:01:11 +0100

    as good as it gets

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -34,6 +34,7 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
+                'label' => 'Password',
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
