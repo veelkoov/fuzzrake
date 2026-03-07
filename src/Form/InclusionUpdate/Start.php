b167ff40796953b528fce246c270351709fe7857
Veelkoov <veelkoov@protonmail.com>
2026-03-07 10:06:52 +0100

    bap

--- src/Form/InclusionUpdate/Start.php
+++ src/Form/InclusionUpdate/Start.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Form\InclusionUpdate;
 
 use App\Controller\IuForm\Utils\StartData;
+use App\Utils\Enforce;
 use Override;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -16,6 +17,8 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 final class Start extends AbstractType
 {
+    public const string OPT_STUDIO_NAME = 'studio_name';
+
     #[Override]
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -39,7 +42,56 @@ final class Start extends AbstractType
                 'expanded'   => true,
                 'required'   => true,
             ])
+            ->add('confirmYouAreTheCreator', ChoiceType::class, [
+                'label'      => 'Are you the maker (studio member)?',
+                'choices'    => [
+                    'I am the maker'                                  => 'i-am-the-creator',
+                    "I'm a friend or a customer or other/non-related" => 'not-the-creator',
+                ],
+                'expanded'   => true,
+                'required'   => true,
+            ])
         ;
+
+        if (null === $options[self::OPT_STUDIO_NAME]) {
+            $builder
+                ->add('confirmAddingANewOne', ChoiceType::class, [
+                    'label'   => 'You are about to request adding a new studio/maker. Is this right?',
+                    'choices' => [
+                        'Yes'                                 => 'yes',
+                        'No, I want to update a maker/studio' => 'no',
+                    ],
+                    'expanded' => true,
+                    'required' => true,
+                ])
+                ->add('ensureStudioIsNotThereAlready', ChoiceType::class, [
+                    'label'      => 'You could already be on the list even if you haven\'t ever sent any form. Please navigate to the main page using the <i class="fa-solid fa-filter-circle-xmark"></i> link above. Check your old names as well.',
+                    'label_html' => true,
+                    'choices'    => [
+                        "Checked after clicking the link above - I'm not there" => 'is-new-studio',
+                        "I've found my old name/studio"                         => 'found-old-studio',
+                    ],
+                    'expanded'  => true,
+                    'required'  => true,
+                ])
+            ;
+        } else {
+            $studioName = htmlspecialchars(Enforce::string($options[self::OPT_STUDIO_NAME]));
+
+            $builder
+                ->add('confirmUpdatingTheRightOne', ChoiceType::class, [
+                    'label'      => "Is this the studio/maker you want to update: <em>$studioName</em>?",
+                    'label_html' => true,
+                    'choices'    => [
+                        'Yes'                                          => 'correct',
+                        'No, that is not the one I want to update'     => 'update-other-one',
+                        'No, instead I want to add a new maker/studio' => 'add-new-instead',
+                    ],
+                    'expanded' => true,
+                    'required' => true,
+                ])
+            ;
+        }
     }
 
     #[Override]
@@ -52,5 +104,11 @@ final class Start extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefault('data_class', StartData::class);
+
+        $resolver
+            ->define(self::OPT_STUDIO_NAME)
+            ->allowedTypes('string', 'null')
+            ->required()
+        ;
     }
 }
