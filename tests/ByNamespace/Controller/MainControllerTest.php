de391233ea895099d57aa430d682829a23561009
Veelkoov <veelkoov@protonmail.com>
2026-04-03 14:06:31 +0200

    fix

--- tests/ByNamespace/Controller/MainControllerTest.php
+++ tests/ByNamespace/Controller/MainControllerTest.php
@@ -5,63 +5,48 @@ declare(strict_types=1);
 namespace App\Tests\ByNamespace\Controller;
 
 use App\Tests\TestUtils\Cases\FuzzrakeWebTestCase;
-use App\Tests\TestUtils\Cases\Traits\FiltersTestTrait;
-use App\Tests\TestUtils\FiltersData;
-use App\Utils\Creator\SmartAccessDecorator as Creator;
-use PHPUnit\Framework\Attributes\DataProvider;
+use App\Tests\TestUtils\UserCreator;
+use App\Utils\DateTime\DateTimeException;
+use App\Utils\DateTime\UtcClock;
 use PHPUnit\Framework\Attributes\Medium;
-use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\Clock\Test\ClockSensitiveTrait;
 
 #[Medium]
 class MainControllerTest extends FuzzrakeWebTestCase
 {
-    use FiltersTestTrait;
+    use ClockSensitiveTrait;
 
-    /**
-     * @param list<Creator>                    $creators
-     * @param array<string, list<string>|bool> $filtersSet
-     * @param list<string>                     $expectedCreatorIds
-     */
-    #[DataProvider('filterChoicesDataProvider')]
-    public function testFiltersThroughHtmx(array $creators, array $filtersSet, array $expectedCreatorIds): void
+    public function testMainPageLoads(): void
     {
-        self::persistAndFlush(...$creators, ...FiltersData::entitiesFrom($creators));
+        self::persistAndFlush(UserCreator::get(true));
 
-        $queryParts = [];
+        self::$client->request('GET', '/');
 
-        foreach ($filtersSet as $name => $value) {
-            if (is_bool($value)) {
-                $queryParts[] = $name.'='.(int) $value;
-            } else {
-                foreach ($value as $item) {
-                    $queryParts[] = $name.'[]='.urlencode($item);
-                }
-            }
-        }
-
-        if (!array_key_exists('wantsSfw', $filtersSet)) {
-            $queryParts[] = 'wantsSfw=0';
-        }
-
-        if (!array_key_exists('isAdult', $filtersSet)) {
-            $queryParts[] = 'isAdult=1';
-        }
-
-        $query = implode('&', $queryParts);
-
-        self::$client->request('GET', '/htmx/main/creators-in-table?'.$query);
         self::assertResponseStatusCodeIs(200);
-
-        $resultCreatorIds = $this->getCrawlerForPartialTableHtml(self::$client->getResponse()->getContent())
-            ->filter('td.creator-id')->each(static fn (Crawler $node, $_) => $node->text(''));
-
-        self::assertSameItems($expectedCreatorIds, $resultCreatorIds, "$query query failed.");
+        self::assertSelectorTextContains('#main-page-intro h4', 'Fursuit makers database');
     }
 
-    private function getCrawlerForPartialTableHtml(string|false $responseContent): Crawler
+    /**
+     * @throws DateTimeException
+     */
+    public function testRecentlyAddedPage(): void
     {
-        self::assertIsString($responseContent);
+        self::mockTime();
 
-        return new Crawler('<!DOCTYPE html> <html lang="en"> <table>'.$responseContent.'</table></html>');
+        $creator1 = UserCreator::get()->setCreatorId('TEST001')->setName('Older creator 1')->setDateAdded(UtcClock::at('-43 days'));
+        $creator2 = UserCreator::get()->setCreatorId('TEST002')->setName('Newer creator 2')->setDateAdded(UtcClock::at('-41 days'));
+        $creator3 = UserCreator::get()->setCreatorId('TEST003')->setName('Newer creator 3')->setDateAdded(UtcClock::at('-40 days'))
+            ->setFormerly(['Formerly 3A', 'Formerly 3B']);
+
+        self::persistAndFlush($creator1, $creator2, $creator3);
+        $this->clearCache();
+
+        $crawler = self::$client->request('GET', '/new');
+        self::assertResponseStatusCodeIs(200);
+
+        self::assertEmpty($crawler->filterXPath('//li/a[text() = "Older creator 1"]'));
+        self::assertNotEmpty($crawler->filterXPath('//li/a[text() = "Newer creator 2"]'));
+        self::assertNotEmpty($crawler->filterXPath('//li/a[text() = "Newer creator 3"]'));
+        self::assertNotEmpty($crawler->filterXPath('//li/span[normalize-space(text()) = "/ Formerly 3A / Formerly 3B"]'));
     }
 }
