3ff1075fae5d03a330f131e30671587dc1444314
Veelkoov <veelkoov@protonmail.com>
2026-04-12 15:46:58 +0200

    Fix false-positive error messages

--- assets/scripts/entry/submissions.ts
+++ assets/scripts/entry/submissions.ts
@@ -1,5 +1,7 @@
 import "../../styles/submissions.scss";
-import { requireJQ } from "../jQueryUtils";
+
+// Not using requireJQ because this is included in all submission management/review pages.
+// Possible improvement here.
 
 jQuery(() => {
   setupDirectiveButtons(
@@ -14,14 +16,14 @@ function setupDirectiveButtons(
   buttonsSelector: string,
   directiveToAdd: (field: string, value: string) => string,
 ) {
-  requireJQ(buttonsSelector, 1, null).on("click", function (event) {
+  jQuery(buttonsSelector).on("click", function (event) {
     const $valueRow = jQuery(event.target).parents("#submission-manage tr");
     const addedDirectives = directiveToAdd(
       $valueRow.data("field"),
       $valueRow.data("value"),
     );
 
-    const directivesTextarea = requireJQ("#manage_directives");
+    const directivesTextarea = jQuery("#manage_directives");
     const currentDirectives = (directivesTextarea.val() || "")
       .toString()
       .trim();
@@ -32,10 +34,10 @@ function setupDirectiveButtons(
 }
 
 function setupOpenAllNewLinksButton() {
-  requireJQ("#open-all-new-links").on("click", function (event) {
+  jQuery("#open-all-new-links").on("click", function (event) {
     event.preventDefault();
 
-    requireJQ('tr.after.changing[data-field^="URL_"]')
+    jQuery('tr.after.changing[data-field^="URL_"]')
       .map((_, domElement) => (domElement.dataset["value"] || "").split(/\s+/))
       .each((index, url) => {
         window.open(url, `url_window_${index}`);
