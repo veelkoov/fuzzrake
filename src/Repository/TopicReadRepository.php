c2ffd3032f340a55fccb42f8431c3f5d3d043518
Veelkoov <veelkoov@protonmail.com>
2026-06-11 20:08:12 +0200

    fix repo

--- src/Repository/TopicReadRepository.php
+++ src/Repository/TopicReadRepository.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Repository;
 
 use App\Entity\TopicRead;
@@ -15,29 +17,4 @@ class TopicReadRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, TopicRead::class);
     }
-
-    //    /**
-    //     * @return TopicRead[] Returns an array of TopicRead objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('t')
-    //            ->andWhere('t.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('t.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?TopicRead
-    //    {
-    //        return $this->createQueryBuilder('t')
-    //            ->andWhere('t.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
