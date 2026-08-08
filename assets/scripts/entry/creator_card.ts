9a6b5422c4b16eda65a864660cc06c6dd4a5aa44
Veelkoov <veelkoov@protonmail.com>
2026-08-08 23:33:21 +0200

    wip

--- assets/scripts/entry/creator_card.ts
+++ assets/scripts/entry/creator_card.ts
@@ -1,1 +1,11 @@
 import "../../styles/creator_card.scss";
+import MessageBus from "../class/MessageBus";
+import { localizeDateTimes } from "../datetimes";
+
+MessageBus.listen('creators-page-loaded', () => {
+  jQuery('div.creator-card-in-list button.toggle').on('click', function() {
+    jQuery(this).parents('div.creator-card-in-list').toggleClass('expanded');
+  });
+
+  localizeDateTimes(jQuery(".creator-card-in-list, .creator-card-standalone"));
+});
