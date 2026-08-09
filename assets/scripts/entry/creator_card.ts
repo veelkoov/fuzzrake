837f42cfe13b54d250e6e90820b79c1cefd5c584
Veelkoov <veelkoov@protonmail.com>
2026-08-09 11:28:54 +0200

    simplify

--- assets/scripts/entry/creator_card.ts
+++ assets/scripts/entry/creator_card.ts
@@ -3,9 +3,9 @@ import MessageBus from "../class/MessageBus";
 import { localizeDateTimes } from "../datetimes";
 
 MessageBus.listen('creators-page-loaded', () => {
-  jQuery('div.creator-card-in-list button.toggle').on('click', function() {
-    jQuery(this).parents('div.creator-card-in-list').toggleClass('expanded');
+  jQuery('div.creator-card button.toggle').on('click', function() {
+    jQuery(this).parents('div.creator-card').toggleClass('expanded');
   });
 
-  localizeDateTimes(jQuery(".creator-card-in-list, .creator-card-standalone"));
+  localizeDateTimes(jQuery(".creator-card"));
 });
