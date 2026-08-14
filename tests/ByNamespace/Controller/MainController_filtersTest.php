d4df032457a180c6083d94735b69ce29b79f9547
Veelkoov <veelkoov@protonmail.com>
2026-08-14 11:34:08 +0200

    wip

--- tests/ByNamespace/Controller/MainController_filtersTest.php
+++ tests/ByNamespace/Controller/MainController_filtersTest.php
@@ -49,11 +49,11 @@ class MainController_filtersTest extends FuzzrakeWebTestCase
 
         $query = implode('&', $queryParts);
 
-        self::$client->request('GET', '/htmx/main/creators-in-table?'.$query);
+        self::$client->request('GET', '/htmx/main/creators-list?'.$query);
         self::assertResponseStatusCodeIs(200);
 
         $resultCreatorIds = $this->getCrawlerForPartialTableHtml(self::$client->getResponse()->getContent())
-            ->filter('td.creator-id')->each(static fn (Crawler $node, $_) => $node->text(''));
+            ->filter('span.creator-id')->each(static fn (Crawler $node, $_) => $node->text(''));
 
         self::assertSameItems($expectedCreatorIds, $resultCreatorIds, "$query query failed.");
     }
