2590bce3ada5006973845e945bd30c58eb7d9a07
Veelkoov <veelkoov@protonmail.com>
2026-09-25 21:10:32 +0200

    chore: refactor around forms w/deletion

--- src/Form/Mx/AbstractTypeWithDelete.php
+++ src/Form/Mx/AbstractTypeWithDelete.php
@@ -17,10 +17,10 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 abstract class AbstractTypeWithDelete extends AbstractType
 {
-    public const string BTN_SAVE = 'save';
-    public const string BTN_DELETE = 'delete';
+    final public const string BTN_SAVE = 'save';
+    final public const string BTN_DELETE = 'delete';
 
-    public const string OPT_DELETABLE = 'deletable';
+    final public const string OPT_DELETABLE = 'deletable';
 
     #[Override]
     public function buildForm(FormBuilderInterface $builder, array $options): void
